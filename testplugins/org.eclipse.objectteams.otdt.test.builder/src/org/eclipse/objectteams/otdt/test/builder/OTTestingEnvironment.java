0573655dfac773037e04f80017240b0dfc6e8fb6
Stephan Herrmann <stephan@cs.tu-berlin.de>
2010-04-02 21:01:47 +0000

    avoid maintaining a branch of org.eclipse.jdt.core.tests.builder by applying this tiny adaption using a team.

--- testplugins/org.eclipse.objectteams.otdt.test.builder/src/org/eclipse/objectteams/otdt/test/builder/OTTestingEnvironment.java
+++ testplugins/org.eclipse.objectteams.otdt.test.builder/src/org/eclipse/objectteams/otdt/test/builder/OTTestingEnvironment.java
@@ -24,19 +24,32 @@ import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IProjectDescription;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jdt.core.JavaCore;
-import org.eclipse.jdt.core.tests.builder.TestingEnvironment;
+import base org.eclipse.jdt.core.tests.builder.TestingEnvironment;
 
-public class OTTestingEnvironment extends TestingEnvironment {
+/** Purpose: add OTJ_NATURE to test projects (method not normally overridable). */
+public team class OTTestingEnvironment {
+	
+	protected class TestingEnvironment playedBy TestingEnvironment {
 
-	@Override // in order to set OT nature: // FIXME: request jdt.method to become protected or use a role for overriding
-	protected void addBuilderSpecs(String projectName) {
-		try {
-			IProject project = getProject(projectName);
-			IProjectDescription description = project.getDescription();
-			description.setNatureIds(new String[] { JavaCore.NATURE_ID, JavaCore.OTJ_NATURE_ID });
-			project.setDescription(description, null);
-		} catch (CoreException e) {
-			handleCoreException(e);
+		@SuppressWarnings("decapsulation")
+		void handleCoreException(CoreException e) -> void handleCoreException(CoreException e);
+
+		IProject getProject(String projectName) -> IProject getProject(String projectName);
+
+		@SuppressWarnings("basecall")
+		callin void addBuilderSpecs(String projectName) {
+			try {
+				IProject project = getProject(projectName);
+				IProjectDescription description = project.getDescription();
+				description.setNatureIds(new String[] { JavaCore.NATURE_ID, JavaCore.OTJ_NATURE_ID });
+				project.setDescription(description, null);
+			} catch (CoreException e) {
+				handleCoreException(e);
+			}
 		}
+
+		@SuppressWarnings("decapsulation")
+		addBuilderSpecs <- replace addBuilderSpecs;
+		
 	}
 }
