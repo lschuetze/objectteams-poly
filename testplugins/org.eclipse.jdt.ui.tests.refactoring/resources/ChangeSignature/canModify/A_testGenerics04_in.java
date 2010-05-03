411c8ff6811321bd70b7872a8e6af4f7811f86a1
Stephan Herrmann <stephan@cs.tu-berlin.de>
2010-05-03 18:33:24 +0000

    re-apply our change: avoid using keyword "as" as an identifier.

--- testplugins/org.eclipse.jdt.ui.tests.refactoring/resources/ChangeSignature/canModify/A_testGenerics04_in.java
+++ testplugins/org.eclipse.jdt.ui.tests.refactoring/resources/ChangeSignature/canModify/A_testGenerics04_in.java
@@ -4,15 +4,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 class A<E> {
-	public <T extends Number> void m(List<Integer> li, A<String> as) {}
+	public <T extends Number> void m(List<Integer> li, A<String> a_of_string) {}
 }
 
 class Sub<E> extends A<E> {
-	public <T extends Number> void m(List<Integer> li, A<String> as) {}
+	public <T extends Number> void m(List<Integer> li, A<String> a_of_string) {}
 	
 	void test() {
-		A<String> as= new A<String>();
-		as.m(new ArrayList<Integer>(1), as);
-		new Sub<Double>().m(new ArrayList<Integer>(2), as);
+		A<String> a_of_string= new A<String>();
+		a_of_string.m(new ArrayList<Integer>(1), a_of_string);
+		new Sub<Double>().m(new ArrayList<Integer>(2), a_of_string);
 	}
 }
--- testplugins/org.eclipse.jdt.ui.tests.refactoring/resources/ChangeSignature/canModify/A_testGenerics04_out.java
+++ testplugins/org.eclipse.jdt.ui.tests.refactoring/resources/ChangeSignature/canModify/A_testGenerics04_out.java
@@ -3,15 +3,15 @@ package p;
 import java.util.List;
 
 class A<E> {
-	public <T extends Number> void m(A<String> as, List<Integer> li) {}
+	public <T extends Number> void m(A<String> a_of_string, List<Integer> li) {}
 }
 
 class Sub<E> extends A<E> {
-	public <T extends Number> void m(A<String> as, List<Integer> li) {}
+	public <T extends Number> void m(A<String> a_of_string, List<Integer> li) {}
 	
 	void test() {
-		A<String> as= new A<String>();
-		as.m(as, null);
-		new Sub<Double>().m(as, null);
+		A<String> a_of_string= new A<String>();
+		a_of_string.m(a_of_string, null);
+		new Sub<Double>().m(a_of_string, null);
 	}
 }
