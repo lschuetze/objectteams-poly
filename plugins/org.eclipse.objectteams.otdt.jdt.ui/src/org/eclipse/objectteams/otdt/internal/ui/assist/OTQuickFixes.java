b6a2ee268ed46faeb7aedd1e6900e99f00de66d9
Stephan Herrmann <stephan@cs.tu-berlin.de>
2010-06-13 09:33:04 +0000

    keyword expansion.

--- plugins/org.eclipse.objectteams.otdt.jdt.ui/src/org/eclipse/objectteams/otdt/internal/ui/assist/OTQuickFixes.java
+++ plugins/org.eclipse.objectteams.otdt.jdt.ui/src/org/eclipse/objectteams/otdt/internal/ui/assist/OTQuickFixes.java
@@ -7,7 +7,7 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * $Id: OTQuickFixes.java 23438 2010-02-04 20:05:24Z stephan $
+ * $Id$
  * 
  * Please visit http://www.eclipse.org/objectteams for updates and contact.
  * 
