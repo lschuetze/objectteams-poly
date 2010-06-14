56942e80c4a7ff0e447c823c01e7bb1c46a63e99
Stephan Herrmann <stephan@cs.tu-berlin.de>
2010-06-14 19:00:17 +0000

    start to manage some more scripts that are not part of the automated build. This file performs various kinds of post-processing on the generated repository.

---
+++ releng/build-scripts/bin/createRepository.sh
@@ -0,0 +1,113 @@
+#!/bin/sh
+
+BASE=/shared/tools/objectteams
+STAGINGBASE=/opt/public/download-staging.priv/tools/objectteams
+
+# Find the master repository to build upon:
+MASTER=${HOME}/downloads/objectteams/updates/$1
+if [ -r ${MASTER}/features ]
+then
+    echo "Generating Repository based on ${MASTER}"
+else
+    echo "No such repository ${MASTER}"
+    echo "Usage: $0 updateMasterRelativePath [ -nosign ]"
+    exit 1
+fi
+
+# Analyze the version number of the JDT feature as needed for patching content.xml later:
+JDTFEATURE=`ls -d ${BASE}/testrun/build-root/eclipse/features/org.eclipse.jdt_*`
+JDTVERSION=`echo ${JDTFEATURE} | cut -d '_' -f 2`
+JDTVERSIONA=`echo ${JDTVERSION} | cut -d '-' -f 1`
+JDTVERSIONB=`echo ${JDTVERSION} | cut -d '-' -f 2`
+JDTVERSIONB2=`expr $JDTVERSIONB + 1`
+JDTVERSIONB2=`printf "%04d" ${JDTVERSIONB2}`
+echo "JDT feature is ${JDTVERSIONA}-${JDTVERSIONB}"
+if [ ! -r ${BASE}/testrun/build-root/eclipse/features/org.eclipse.jdt_${JDTVERSIONA}-${JDTVERSIONB}-* ]
+then
+    echo "JDT feature not correctly found in ${BASE}/testrun/build-root/eclipse/features"
+    exit 2
+fi
+
+
+# Configure for calling various p2 applications:
+LAUNCHER=`grep equinox.launcher_jar= ${BASE}/build/run.properties | cut -d '=' -f 2`
+LAUNCHER_PATH=${BASE}/testrun/build-root/eclipse/plugins/${LAUNCHER}
+FABPUB=org.eclipse.equinox.p2.publisher.FeaturesAndBundlesPublisher
+CATPUB=org.eclipse.equinox.p2.publisher.CategoryPublisher
+NAME="Object Teams"
+
+echo "LAUNCHER_PATH = ${LAUNCHER_PATH}"
+echo "NAME          = ${NAME}"
+
+echo "====Step 1: zip and request signing===="
+cd ${BASE}/testrun/updateSite
+JARS=`find . -name \*.jar -type f`
+zip ${STAGINGBASE}/in/otdt.zip ${JARS}
+if [ "$2" == "-nosign" ]
+then
+    echo "SKIPING SIGNING"
+else
+    /bin/rm ${STAGINGBASE}/out/otdt.zip
+    sign ${STAGINGBASE}/in/otdt.zip nomail ${STAGINGBASE}/out
+fi
+until [ -r ${STAGINGBASE}/out/otdt.zip ]
+do
+    sleep 10
+    echo -n "."
+done
+echo "Signing completed"
+
+
+echo "====Step 2: fill new repository===="
+if [ -r ${BASE}/stagingRepo ]
+then
+    /bin/rm -rf ${BASE}/stagingRepo
+fi
+mkdir ${BASE}/stagingRepo
+cd ${BASE}/stagingRepo
+mkdir features
+(cd features; ln -s ${MASTER}/features/* .)
+mkdir plugins
+(cd plugins; ln -s ${MASTER}/plugins/* .)
+unzip ${STAGINGBASE}/out/otdt.zip
+
+LOCATION=${BASE}/stagingRepo
+echo "LOCATION  = ${LOCATION}"
+cd ${LOCATION}
+
+
+echo "====Step 3: generate metadata===="
+java -jar ${LAUNCHER_PATH} -consoleLog -application ${FABPUB} \
+    -source ${LOCATION} \
+    -metadataRepository file:${LOCATION} \
+    -artifactRepository file:${LOCATION} \
+    -metadataRepositoryName "${NAME} Updates" \
+    -artifactRepositoryName "${NAME} Artifacts"
+ls -ltr *\.*
+
+
+echo "====Step 4: generate category===="
+CATEGORYARGS="-categoryDefinition file:${BASE}/testrun/build-root/src/features/org.eclipse.objectteams.otdt/category.xml"
+echo "CATEGORYARGS  = ${CATEGORYARGS}"
+java -jar ${LAUNCHER_PATH} -consoleLog -application ${CATPUB} \
+    -source ${LOCATION} \
+    -metadataRepository file:${LOCATION} \
+    ${CATEGORYARGS}
+ls -ltr *\.*
+
+
+echo "====Step 5: patch content for feature inclusion version range===="
+mv content.xml content.xml-orig
+xsltproc  -o content.xml --stringparam version ${JDTVERSIONA}-${JDTVERSIONB} \
+    --stringparam versionnext ${JDTVERSIONA}-${JDTVERSIONB2} \
+    ../build/patch-content-xml.xsl content.xml-orig
+ls -ltr *\.*
+
+
+echo "Step 6: jar-up metadata"
+jar cf content.jar content.xml
+jar cf artifacts.jar artifacts.xml
+/bin/rm *.xml
+ls -ltr *\.*
+
+echo "====DONE===="
