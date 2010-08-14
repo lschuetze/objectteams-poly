51623e35464d297082753180224241a56cdc0f54
Stephan Herrmann <stephan@cs.tu-berlin.de>
2010-08-14 16:00:19 +0000

    Cleanup/migrating for  Bug 321411 -  [hierarchy] Replace OTTypeHierarchy with adapting the original TypeHierarchy (see comment 6)

--- plugins/org.eclipse.objectteams.otdt.jdt.ui/src/org/eclipse/objectteams/otdt/internal/ui/typehierarchy/TypeHierarchyViewAdaptor.java
+++ plugins/org.eclipse.objectteams.otdt.jdt.ui/src/org/eclipse/objectteams/otdt/internal/ui/typehierarchy/TypeHierarchyViewAdaptor.java
@@ -23,235 +23,26 @@ package org.eclipse.objectteams.otdt.internal.ui.typehierarchy;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.eclipse.core.runtime.IProgressMonitor;
-
-import org.eclipse.jdt.core.IJavaElement;
-import org.eclipse.jdt.core.IMember;
 import org.eclipse.jdt.core.IType;
 import org.eclipse.jdt.core.ITypeHierarchy;
 import org.eclipse.jdt.core.JavaModelException;
-import org.eclipse.jdt.internal.ui.actions.CompositeActionGroup;
-import org.eclipse.jdt.internal.ui.typehierarchy.MethodsViewer;
-import org.eclipse.jdt.internal.ui.typehierarchy.ToggleViewAction;
-import org.eclipse.jdt.ui.ITypeHierarchyViewPart;
-import org.eclipse.jdt.ui.JavaElementLabels;
-import org.eclipse.objectteams.otdt.core.IMethodMapping;
-import org.eclipse.objectteams.otdt.core.IOTJavaElement;
+import org.eclipse.jdt.internal.ui.typehierarchy.TypeHierarchyLifeCycle;
 import org.eclipse.objectteams.otdt.core.IOTType;
 import org.eclipse.objectteams.otdt.core.IRoleType;
 import org.eclipse.objectteams.otdt.core.OTModelManager;
 import org.eclipse.objectteams.otdt.core.TypeHelper;
-import org.eclipse.objectteams.otdt.core.compiler.IOTConstants;
 import org.eclipse.objectteams.otdt.ui.Util;
 
-import base org.eclipse.jdt.internal.ui.typehierarchy.HierarchyLabelProvider;
 import base org.eclipse.jdt.internal.ui.typehierarchy.MethodsContentProvider;
-import base org.eclipse.jdt.internal.ui.typehierarchy.MethodsLabelProvider;
-import base org.eclipse.jdt.internal.ui.typehierarchy.TypeHierarchyLifeCycle;
-import base org.eclipse.jdt.internal.ui.typehierarchy.TypeHierarchyViewPart;
-import base org.eclipse.jdt.internal.ui.typehierarchy.SubTypeHierarchyViewer.SubTypeHierarchyContentProvider;
 
 /**
- * This team adapts the type hierarchy to handle role inheritance correctly.
+ * This team adapts the type hierarchy to show method mappings.
  * 
  * @author stephan
  */
 @SuppressWarnings("restriction")
 public team class TypeHierarchyViewAdaptor 
 {
-	/** Manipulate set of available actions. */
-	@SuppressWarnings("decapsulation")
-	protected class ViewPart playedBy TypeHierarchyViewPart 
-	{
-		void changeViewActions() 
-		{
-			// remove first action (VIEW_ID_TYPE not applicable in the OT hierarchy)
-			ToggleViewAction[] actions = getViewActions();
-			setViewActions(new ToggleViewAction[] { actions[1], actions[2] });
-		}
-		changeViewActions <- before createPartControl;
-
-		ToggleViewAction[] getViewActions()               -> get ToggleViewAction[] fViewActions;
-		void setViewActions(ToggleViewAction[] actions)   -> set ToggleViewAction[] fViewActions;
-		
-		CompositeActionGroup getActionGroups()            -> get CompositeActionGroup fActionGroups;
-		void setActionGroups(CompositeActionGroup groups) -> set CompositeActionGroup fActionGroups;
-		
-		// ensure the old default value CLASSIC never comes through:
-		callin void setHierarchyMode(int viewerIndex) {
-			if (viewerIndex == ITypeHierarchyViewPart.HIERARCHY_MODE_CLASSIC)
-				viewerIndex = ITypeHierarchyViewPart.HIERARCHY_MODE_SUPERTYPES;
-			base.setHierarchyMode(viewerIndex);
-		}
-		void setHierarchyMode(int viewerIndex) <- replace void setHierarchyMode(int viewerIndex);
-	}
-	
-	/** If element is a role type, ensure an OTTypeHierarchy is created. */
-	protected class LifeCycle playedBy TypeHierarchyLifeCycle 
-	{
-		@SuppressWarnings("basecall")
-		callin ITypeHierarchy createTypeHierarchy(IJavaElement element, IProgressMonitor pm) throws JavaModelException {
-			if (element.getElementType() == IJavaElement.TYPE) {
-				// compute a hierarchy that can handle teams and roles    
-			    IOTType otType = OTModelManager.getOTElement((IType) element);
-			    if (otType != null) {
-				    if (getIsSuperTypesOnly())
-				        return otType.newSuperOTTypeHierarchy(pm);
-				    else
-				        return otType.newOTTypeHierarchy(pm);
-			    }		  
-			}
-			return base.createTypeHierarchy(element, pm);
-		}
-		@SuppressWarnings("decapsulation")
-		createTypeHierarchy <- replace createTypeHierarchy;
-		
-		@SuppressWarnings("decapsulation")
-		boolean getIsSuperTypesOnly() -> get boolean fIsSuperTypesOnly;
-		
-		protected
-		ITypeHierarchy getHierarchy() -> ITypeHierarchy getHierarchy();
-	}
-	
-	
-	/** Refuse to answer _the_ super class for a role type. 
-	 *  Return null rather throwing an exception as OTTypeHierarchy would do. */
-	protected class SubTypeHierarchyContentProvider playedBy SubTypeHierarchyContentProvider 
-	{
-		@SuppressWarnings("basecall")
-		callin Object getParent(Object element) {
-			if(element instanceof IType) {
-				IOTType otType = OTModelManager.getOTElement((IType)element);
-				if (otType != null && otType.isRole())
-					return null; // not a single parent
-			}
-			return base.getParent(element);
-		}
-		getParent <- replace getParent;
-	}
-	
-	/**
-	 * Ensure role names are always prefixed upto and including the enclosing team type. 
-	 */
-	public class HierarchyLabelProvider  playedBy HierarchyLabelProvider 
-	{
-		@SuppressWarnings("basecall")
-		callin String getText(Object element) {
-	        IOTType otType = null;
-			if(element instanceof IType)
-			{
-			    IType type = (IType)element;
-			    if(OTModelManager.hasOTElementFor(type)) {
-			    	otType = OTModelManager.getOTElement((IType)element);
-			    } else {
-			        IType encType = type.getDeclaringType();
-			        IType classPart = 
-			            encType == null ? null : encType.getType(IOTConstants.OT_DELIM + type.getElementName());
-			        if (classPart != null && OTModelManager.hasOTElementFor(classPart))
-			            otType = OTModelManager.getOTElement(classPart);
-			    }
-			}
-			if(otType != null) {    	
-			    //team or role
-			    StringBuffer text = new StringBuffer();
-				computeNames(text, otType);
-				return text.toString();
-			} else {
-			    //ordinary type
-				return base.getText(element);
-			}
-
-		}
-		getText <- replace getText;
-	}
-
-	/**
-	 * Different presentation for role methods, if defining type should be displayed.
-	 * Also support method mappings.
-	 */
-	protected class MethodsLabelProvider playedBy MethodsLabelProvider {
-
-	    callin String getText(Object element) {
-	    	String text;
-	    	boolean isRole = false;
-	    	if (element instanceof IMember) { 
-	    		IType declaringType = ((IMember)element).getDeclaringType();
-	    		IOTType otType = OTModelManager.getOTElement(declaringType);
-                if(otType != null && otType instanceof IRoleType)
-                	isRole = true;
-	    	}
-	        if(isRole && isShowDefiningType())  {
-	        	// get the text without defining type, because we need to
-	        	// compute the defining type for roles ourselves:
-	            setShowDefiningType(false);
-	            text= base.getText(element);
-	            setShowDefiningType(true);
-
-	            try {
-	            	// compute and assemble the defining type name:
-	                IType type = otGetDefiningType(element);
-		            if(type != null) {
-		                StringBuffer extText = new StringBuffer();
-		                IOTType otEquiv = isRole ? 
-		                		OTModelManager.getOTElement(type) :
-		                		null;
-		                if(otEquiv != null && otEquiv instanceof IRoleType) {
-		                	// prepend qualified role name:
-		                    computeNames(extText, otEquiv);
-		                } else {
-		                	extText.append(type.getElementName());
-		                }
-		                extText.append(JavaElementLabels.CONCAT_STRING);
-		                extText.append(text);
-		                return extText.toString();
-		            }
-	            }
-	            catch (JavaModelException exc) { /* Just use the text we already have. */ }
-	        } else {
-	        	text= base.getText(element);
-	        }
-	        
-	        if (element instanceof IMethodMapping && getShowInherited() && !isShowDefiningType()) {
-	        	// this case is not handled above:
-	        	// method mappings needing to show their declaring type.
-	            StringBuffer extText = new StringBuffer();
-	            extText.append(text);
-	            extText.append(JavaElementLabels.CONCAT_STRING);
-	            extText.append(((IMethodMapping)element).getDeclaringType().getFullyQualifiedName('.'));
-	            return extText.toString();
-	        }
-	            
-	        return text;
-	    }
-	    getText <- replace getText;
-	    
-	    
-	    private IType otGetDefiningType(Object element) throws JavaModelException 
-		{
-	        if (element instanceof IOTJavaElement) { 
-		        switch (((IOTJavaElement)element).getElementType()) {
-		        case IOTJavaElement.CALLIN_MAPPING:
-		        case IOTJavaElement.CALLOUT_MAPPING:
-		        case IOTJavaElement.CALLOUT_TO_FIELD_MAPPING:
-		        	// FIXME(SH): inconsistency: should search the initial defining type instead:
-		        	return ((IMethodMapping)element).getDeclaringType();
-		        }
-	        }
-	        return getDefiningType(element);
-		}
-
-	    abstract IType getDefiningType(Object element) throws JavaModelException;
-	    
-	    @SuppressWarnings("decapsulation")
-		IType getDefiningType(Object element)              -> IType getDefiningType(Object element);
-	    void setShowDefiningType(boolean showDefiningType) -> void setShowDefiningType(boolean showDefiningType);
-	    boolean isShowDefiningType()                       -> boolean isShowDefiningType();
-	    
-	    @SuppressWarnings("decapsulation")
-	    boolean getShowInherited() -> get MethodsViewer fMethodsViewer
-	    	with {          result <- fMethodsViewer.isShowInheritedMethods() }
-	}
-
 	private static final Object[] NO_ELEMENTS = new Object[0];
 	
 	/** Filter generated elements and also provide method mappings. */
@@ -321,27 +112,8 @@ public team class TypeHierarchyViewAdaptor
 		}		
 		
 		@SuppressWarnings("decapsulation")
-		LifeCycle getHierarchyLifeCycle() -> get TypeHierarchyLifeCycle fHierarchyLifeCycle;
+		TypeHierarchyLifeCycle getHierarchyLifeCycle() -> get TypeHierarchyLifeCycle fHierarchyLifeCycle;
 
 		boolean getShowInheritedMethods() -> boolean isShowInheritedMethods();
 	}
-	
-	
-	/**
-	 * Helper for two roles: create a role name prefixed with its team
-	 * (and all intermediate roles, if nested). 
-	 */
-    private static StringBuffer computeNames(StringBuffer buffer, IOTType type)
-    {
-        IOTType curType = type;
-        while(curType.isRole())
-        {
-            buffer.insert(0, curType.getElementName());
-            buffer.insert(0, '.');
-            curType = ((IRoleType)curType).getTeam();
-            if (curType == null) 
-            	return buffer; // avoid NPE, leave incomplete name if needed
-        }
-        return buffer.insert(0, curType.getElementName());
-    }
 }
--- plugins/org.eclipse.objectteams.otdt.jdt.ui/src/org/eclipse/objectteams/otdt/internal/ui/util/HierarchyUtilAdaptor.java
+++ plugins/org.eclipse.objectteams.otdt.jdt.ui/src/org/eclipse/objectteams/otdt/internal/ui/util/HierarchyUtilAdaptor.java
@@ -16,34 +16,16 @@
  **********************************************************************/
 package org.eclipse.objectteams.otdt.internal.ui.util;
 
-import java.util.Iterator;
-import java.util.LinkedList;
+import java.util.List;
 
-import org.eclipse.jdt.core.Flags;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.jdt.core.IJavaElement;
-import org.eclipse.jdt.core.IMethod;
-import org.eclipse.jdt.core.IType;
-import org.eclipse.jdt.core.ITypeHierarchy;
-import org.eclipse.jdt.core.ITypeParameter;
-import org.eclipse.jdt.core.JavaModelException;
-import org.eclipse.jdt.core.Signature;
-import org.eclipse.jdt.internal.ui.typehierarchy.TypeHierarchyLifeCycle;
-import org.eclipse.jface.viewers.IContentProvider;
-import org.eclipse.jface.viewers.ITreePathContentProvider;
-import org.eclipse.jface.viewers.TreePath;
+import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.objectteams.otdt.core.IOTJavaElement;
-import org.eclipse.objectteams.otdt.core.IOTType;
-import org.eclipse.objectteams.otdt.core.OTModelManager;
-import org.eclipse.objectteams.otdt.internal.core.OTType;
-import org.eclipse.objectteams.otdt.internal.core.OTTypeHierarchy;
-import org.eclipse.swt.widgets.Item;
-import org.eclipse.swt.widgets.Widget;
 
-import base org.eclipse.jdt.internal.corext.util.JavaModelUtil;
-import base org.eclipse.jdt.internal.corext.util.MethodOverrideTester;
-import base org.eclipse.jdt.internal.corext.util.MethodOverrideTester.Substitutions;
-import base org.eclipse.jdt.internal.ui.typehierarchy.TraditionalHierarchyViewer.TraditionalHierarchyContentProvider;
 import base org.eclipse.jdt.internal.ui.util.OpenTypeHierarchyUtil;
+import base org.eclipse.jdt.ui.actions.OpenTypeHierarchyAction;
 
 /**
  * This team adapts different utility classes relating to hierarchies.
@@ -54,7 +36,7 @@ import base org.eclipse.jdt.internal.ui.util.OpenTypeHierarchyUtil;
 public team class HierarchyUtilAdaptor 
 {
 	/** Also consider method mappings as candidates for a hierarchy. */ 
-	protected class OpenTypeHierarchy playedBy OpenTypeHierarchyUtil 
+	protected class OpenTypeHierarchyUtil playedBy OpenTypeHierarchyUtil 
 	{		
 		@SuppressWarnings("basecall")
 		static callin IJavaElement[] getCandidates(Object input) {
@@ -72,172 +54,47 @@ public team class HierarchyUtilAdaptor
 		getCandidates <- replace getCandidates;
 	}
 	
-	/** Consider multiple super classes for roles. */
-	@SuppressWarnings("decapsulation"/*final baseclass*/) 
-	protected class JavaModelUtil playedBy JavaModelUtil 
-	{
-		@SuppressWarnings("basecall")
-		static callin boolean isSuperType(ITypeHierarchy hierarchy, IType possibleSuperType, IType type)
-		{
-			if (hierarchy instanceof OTTypeHierarchy) {
-		        IOTType otType = OTModelManager.getOTElement(type);
-		        if (otType != null && otType.isRole())
-				{
-					IType[] supertypes = ((OTTypeHierarchy)hierarchy).getAllSupertypes(type);
-					for (IType supertype : supertypes) 
-						if (supertype.equals(possibleSuperType))
-							return true;
+	/** Enable the open type hiearchy action for method mappings. */
+	protected class OpenTypeHierarchyAction playedBy OpenTypeHierarchyAction {
+
+		@SuppressWarnings("decapsulation")
+		boolean isEnabled(IStructuredSelection selection) <- replace boolean isEnabled(IStructuredSelection selection);
+
+		callin boolean isEnabled(IStructuredSelection selection) {
+			if (base.isEnabled(selection))
+				return true;
+			
+			if (selection.size() != 1)
+				return false;
+			Object input= selection.getFirstElement();
+			if (!(input instanceof IOTJavaElement))
+				return false;
+			switch (((IOTJavaElement)input).getElementType()) {
+				case IOTJavaElement.CALLIN_MAPPING:
+				case IOTJavaElement.CALLOUT_MAPPING:
+				case IOTJavaElement.CALLOUT_TO_FIELD_MAPPING:
+					return true;
+				default:
 					return false;
-				}
 			}
-			return base.isSuperType(hierarchy, possibleSuperType, type);
-		}
-		isSuperType <- replace isSuperType;
-	}
-	
-	/** Find overridden methods in all kinds of super types. */
-	protected class MethodOverrideTester playedBy MethodOverrideTester 
-	{
-		abstract IMethod findOverriddenMethodInType(IType overriddenType, IMethod overriding)
-			throws JavaModelException;
-		
-		findOverriddenMethodInType -> findOverriddenMethodInType;
-
-		@SuppressWarnings("decapsulation")
-		ITypeHierarchy getHierarchy() -> get ITypeHierarchy fHierarchy;
-
-		
-		@SuppressWarnings("basecall")
-		callin IMethod findOverriddenMethod(IMethod overriding, boolean testVisibility) 
-			throws JavaModelException 
-		{
-			ITypeHierarchy hierarchy = getHierarchy();
-			if (hierarchy instanceof OTTypeHierarchy) 
-			{
-				IType declaringType = overriding.getDeclaringType();
-				IOTType declaringOTType = OTModelManager.getOTElement(declaringType);
-				if (declaringOTType != null && declaringOTType.isRole()) 
-				{
-					int flags= overriding.getFlags();
-					if (Flags.isPrivate(flags) || Flags.isStatic(flags) || overriding.isConstructor()) {
-						return null;
-					}
-					
-					IType[] supertypes = ((OTTypeHierarchy)hierarchy).getAllSupertypes(declaringType);
-					for (IType type : supertypes) {
-						IMethod method = findOverriddenMethodInType(type, overriding);
-						if (method != null)
-							return method;
-					}
-					return null;
-				}
-			}
-			return base.findOverriddenMethod(overriding, testVisibility);
-		}
-		findOverriddenMethod <- replace findOverriddenMethod;
-		
-		// ---- Second Use Case: invoked from Quick-Hierarchy of a role method
-		@SuppressWarnings("decapsulation")
-		void computeOTSubstitutions(IType instantiatedType, IType instantiatingType, String[] typeArguments)
-		<- replace
-		void computeSubstitutions(IType instantiatedType, IType instantiatingType, String[] typeArguments)
-			base when (OTModelManager.isRole(instantiatedType));
-
-		// for recursion go back to the base method
-		@SuppressWarnings("decapsulation")
-		void computeSubstitutions(IType instantiatedType, IType instantiatingType, String[] typeArguments)
-		-> void computeSubstitutions(IType instantiatedType, IType instantiatingType, String[] typeArguments);
-		
-		@SuppressWarnings({ "inferredcallout", "decapsulation", "basecall", "unchecked" })
-		callin void computeOTSubstitutions(IType instantiatedType, IType instantiatingType, String[] typeArguments) 
-				throws JavaModelException 
-		{
-			Substitutions s= new Substitutions();
-			fTypeVariableSubstitutions.put(instantiatedType, s);
-
-			ITypeParameter[] typeParameters= instantiatedType.getTypeParameters();
-
-			if (instantiatingType == null) { // the focus type
-				for (int i= 0; i < typeParameters.length; i++) {
-					ITypeParameter curr= typeParameters[i];
-					// use star to make type variables different from type refs
-					s.addSubstitution(curr.getElementName(), '*' + curr.getElementName(), getTypeParameterErasure(curr, instantiatedType));
-				}
-			} else {
-				if (typeParameters.length == typeArguments.length) {
-					for (int i= 0; i < typeParameters.length; i++) {
-						ITypeParameter curr= typeParameters[i];
-						String substString= getSubstitutedTypeName(typeArguments[i], instantiatingType); // substitute in the context of the instantiatingType
-						String erasure= getErasedTypeName(typeArguments[i], instantiatingType); // get the erasure from the type argument
-						s.addSubstitution(curr.getElementName(), substString, erasure);
-					}
-				} else if (typeArguments.length == 0) { // raw type reference
-					for (int i= 0; i < typeParameters.length; i++) {
-						ITypeParameter curr= typeParameters[i];
-						String erasure= getTypeParameterErasure(curr, instantiatedType);
-						s.addSubstitution(curr.getElementName(), erasure, erasure);
-					}
-				} else {
-					// code with errors
-				}
-			}
-			String superclassTypeSignature= instantiatedType.getSuperclassTypeSignature();
-			if (superclassTypeSignature != null) {
-				String[] superTypeArguments= Signature.getTypeArguments(superclassTypeSignature);
-//{ObjectTeams: account for multiple super classes:
-/* orig:
-				IType superclass= fHierarchy.getSuperclass(instantiatedType);
-				if (superclass != null && !fTypeVariableSubstitutions.containsKey(superclass)) {
-					computeSubstitutions(superclass, instantiatedType, superTypeArguments);
-				}
-  :giro */
-				for (IType superclass : ((OTTypeHierarchy)fHierarchy).getSuperclasses(instantiatedType)) {
-					if (superclass != null && !fTypeVariableSubstitutions.containsKey(superclass)) {
-						computeSubstitutions(superclass, instantiatedType, superTypeArguments);
-					}
-				}
-// SH}
-			}
-			String[] superInterfacesTypeSignature= instantiatedType.getSuperInterfaceTypeSignatures();
-			int nInterfaces= superInterfacesTypeSignature.length;
-			if (nInterfaces > 0) {
-				IType[] superInterfaces= fHierarchy.getSuperInterfaces(instantiatedType);
-				if (superInterfaces.length == nInterfaces) {
-					for (int i= 0; i < nInterfaces; i++) {
-						String[] superTypeArguments= Signature.getTypeArguments(superInterfacesTypeSignature[i]);
-						IType superInterface= superInterfaces[i];
-						if (!fTypeVariableSubstitutions.containsKey(superInterface)) {
-							computeSubstitutions(superInterface, instantiatedType, superTypeArguments);
-						}
-					}
-				}
-			}			
-		}
-	}
-	
-	/** Mere gateway role, confined as to force lowering when assigning to Object. */
-	@SuppressWarnings("decapsulation")
-	protected class Substitutions extends Confined playedBy Substitutions {
-		protected Substitutions() { base(); }
-
-		void addSubstitution(String elementName, String string, String typeParameterErasure)
-		-> void addSubstitution(String elementName, String string, String typeParameterErasure);
-	}
-	
-	/** If a single parent is requested of a role type answer its explicit superclass. */
-	protected class TraditionalHierarchyContent playedBy TraditionalHierarchyContentProvider {
-
-		IType getParentType(IType type) <- replace IType getParentType(IType type)
-			base when (OTModelManager.isRole(type));
-
-		@SuppressWarnings("basecall")
-		callin IType getParentType(IType type) {
-			ITypeHierarchy hierarchy = getTypeHierarchy();
-			return ((OTTypeHierarchy)hierarchy).getExplicitSuperclass(type);
 		}
 
-		@SuppressWarnings("decapsulation")
-		ITypeHierarchy getTypeHierarchy() -> get TypeHierarchyLifeCycle fTypeHierarchy
-			with { result <- result.getHierarchy() }	
+		@SuppressWarnings({ "rawtypes", "decapsulation" })
+		IStatus compileCandidates(List resultlist, IJavaElement elem) <- replace IStatus compileCandidates(List resultlist, IJavaElement elem);
+
+		@SuppressWarnings({ "rawtypes", "unchecked", "basecall" }) // parameter is raw list
+		static callin IStatus compileCandidates(List resultlist, IJavaElement elem) {
+			IStatus ok= Status.OK_STATUS;
+			switch (elem.getElementType()) {
+				case IOTJavaElement.CALLIN_MAPPING:
+				case IOTJavaElement.CALLOUT_MAPPING:
+				case IOTJavaElement.CALLOUT_TO_FIELD_MAPPING:
+					resultlist.add(elem);
+					return ok;
+				default:
+					return base.compileCandidates(resultlist, elem);
+			}
+		}		
 	}
+
 }
