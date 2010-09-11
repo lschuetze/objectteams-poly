cd06dea2a53cda2f97e7d217f6a62c61ec4596e9
Stephan Herrmann <stephan@cs.tu-berlin.de>
2010-09-11 23:45:21 +0000

    one more tiny test fix

--- testplugins/org.eclipse.objectteams.otdt.tests/workspace/ExternalDefinedRole/boundtoteam/boundtoteam/teampkg/Team_5e/SampleRole.java
+++ testplugins/org.eclipse.objectteams.otdt.tests/workspace/ExternalDefinedRole/boundtoteam/boundtoteam/teampkg/Team_5e/SampleRole.java
@@ -6,7 +6,7 @@ import teampkg.TeamB;
  * @author kaschja
  * @version $Id: SampleRole.java 7495 2005-10-18 14:56:37Z carp $
  */
-public class SampleRole playedBy TeamB
+public class SampleRole playedBy TeamC
 {
     public void roleMethod() {}
     roleMethod <- after baseMethod; 
