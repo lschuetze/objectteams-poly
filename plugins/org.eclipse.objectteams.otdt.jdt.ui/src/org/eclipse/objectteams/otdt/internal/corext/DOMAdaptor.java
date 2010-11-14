7ecc7b0c40837a9f9da6a6d1ce05e5bc64ddf85f
Stephan Herrmann <stephan@cs.tu-berlin.de>
2010-11-14 15:05:39 +0000

    Test & fix for Bug 330169 -  [refactoring] Extract method throws NPE if declared lifting arg is involved

--- plugins/org.eclipse.objectteams.otdt.jdt.ui/src/org/eclipse/objectteams/otdt/internal/corext/DOMAdaptor.java
+++ plugins/org.eclipse.objectteams.otdt.jdt.ui/src/org/eclipse/objectteams/otdt/internal/corext/DOMAdaptor.java
@@ -1,7 +1,7 @@
 /**********************************************************************
  * This file is part of "Object Teams Development Tooling"-Software
  * 
- * Copyright 2006, 2007 Technical University Berlin, Germany.
+ * Copyright 2006, 2010 Technical University Berlin, Germany.
  * 
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -18,9 +18,15 @@ package org.eclipse.objectteams.otdt.internal.corext;
 
 import org.eclipse.jdt.core.dom.ASTNode;
 import org.eclipse.jdt.core.dom.CalloutMappingDeclaration;
+import org.eclipse.jdt.core.dom.IVariableBinding;
+import org.eclipse.jdt.core.dom.LiftingType;
 import org.eclipse.jdt.core.dom.RoleTypeDeclaration;
 import org.eclipse.jdt.core.dom.rewrite.ASTRewrite;
 import org.eclipse.jdt.core.dom.rewrite.ListRewrite;
+
+import base org.eclipse.jdt.core.dom.VariableBinding;
+import base org.eclipse.jdt.internal.corext.dom.LocalVariableIndex;
+import base org.eclipse.jdt.internal.corext.dom.ASTFlattener;
 import base org.eclipse.jdt.internal.corext.dom.ModifierRewrite;
 
 /**
@@ -28,12 +34,14 @@ import base org.eclipse.jdt.internal.corext.dom.ModifierRewrite;
  * 
  * Issues:
  * - hard coded switch missing OT-cases.
+ * - visitors unaware of OT-syntax, specifically: declared lifting.
  * 
  * @author stephan
  */
 @SuppressWarnings("restriction")
 public team class DOMAdaptor 
 {
+	
 	protected class ModifierRewrite playedBy ModifierRewrite 
 	{
 		@SuppressWarnings("decapsulation")
@@ -50,6 +58,51 @@ public team class DOMAdaptor
 				return rewrite.getListRewrite(declNode, CalloutMappingDeclaration.MODIFIERS2_PROPERTY);
 			}
 			return base.evaluateListRewrite(rewrite, declNode);
+		}
+	}
+	
+	/** 
+	 * Make this visitor aware of declared lifting.
+	 * 
+	 * TODO: other OT syntax? It seems, this flattener is, e.g, NOT used for classes
+	 *       so playedBy etc. needn't be handled. 
+	 */
+	protected class ASTFlattener playedBy ASTFlattener 
+	{
+		@SuppressWarnings("decapsulation")
+		StringBuffer getFBuffer() -> get StringBuffer fBuffer;
+
+		boolean visit(LiftingType node) <- replace boolean visit(LiftingType node);
+
+		@SuppressWarnings("basecall")
+		callin boolean visit(LiftingType node) {
+			node.getBaseType().accept(this);
+			getFBuffer().append(" as "); //$NON-NLS-1$
+			node.getRoleType().accept(this);
+			return false;
+		}
+	}
+	
+	/** Make this visitor aware of declared lifting. */
+	protected team class LocalVariableIndex playedBy LocalVariableIndex 
+	{
+		/** gateway to package private class (rather than exposing new method via the API interface) */
+		protected class VariableBinding playedBy VariableBinding {
+			int getVariableIdMax() -> int getVariableIdMax();
+		}
+		
+		@SuppressWarnings("decapsulation") int getFTopIndex() -> get int fTopIndex;
+		@SuppressWarnings("decapsulation") void setFTopIndex(int fTopIndex) -> set int fTopIndex;
+
+		@SuppressWarnings("decapsulation")
+		void handleVariableBinding(VariableBinding variableBinding) <- replace void handleVariableBinding(IVariableBinding binding)
+			base when (binding instanceof VariableBinding)
+			with { variableBinding <- (VariableBinding)binding }
+
+		/** An argument with declared lifting has two IDs, use the max of both. */
+		@SuppressWarnings("basecall")
+		callin void handleVariableBinding(VariableBinding binding) {
+			setFTopIndex(Math.max(getFTopIndex(), binding.getVariableIdMax()));
 		}
 	}
 }
