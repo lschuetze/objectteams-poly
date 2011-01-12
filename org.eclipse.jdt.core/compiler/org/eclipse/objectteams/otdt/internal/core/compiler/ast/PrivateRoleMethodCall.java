c4782dc9e7314119a68338ed40ae25d544e7ab91
Stephan Herrmann <stephan@cs.tu-berlin.de>
2011-01-12 08:42:36 +0000

    copyright update/correction

--- org.eclipse.jdt.core/compiler/org/eclipse/objectteams/otdt/internal/core/compiler/ast/PrivateRoleMethodCall.java
+++ org.eclipse.jdt.core/compiler/org/eclipse/objectteams/otdt/internal/core/compiler/ast/PrivateRoleMethodCall.java
@@ -1,17 +1,17 @@
 /** 
  * This file is part of "Object Teams Development Tooling"-Software
  * 
- * Copyright 2009 Stephan Herrmann
+ * Copyright 2011 GK Software AG
  * 
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * $Id: CallinImplementorDyn.java 23417 2010-02-03 20:13:55Z stephan $
  * 
  * Please visit http://www.eclipse.org/objectteams for updates and contact.
  * 
- * Contributors: Stephan Herrmann - Initial API and implementation
+ * Contributors: 
+ * 		Stephan Herrmann - Initial API and implementation
  **********************************************************************/
 package org.eclipse.objectteams.otdt.internal.core.compiler.ast;
 
