dd477c9f6e35b11a96251dc698561385ed491136
Stephan Herrmann <stephan@cs.tu-berlin.de>
2011-01-29 21:01:21 +0000

    versions for 0.8M5

--- releng/build-scripts/build/otdt_prerequisites.sh
+++ releng/build-scripts/build/otdt_prerequisites.sh
@@ -48,11 +48,11 @@ fi
 
 # VERSIONS:
 # Eclipse SKD build identifier:
-EVERSION=3.7M4
+EVERSION=3.7M5
 # Architecture (as used by OSGi):
 ARCH=`arch`
 # used only locally:
-DROP=${BASEDIR}/drops/S-3.7M4-201012081300
+DROP=${BASEDIR}/drops/S-3.7M5-201101272034
 
 # EXPORT: archive file of the base eclipse SDK build:
 ECLIPSE_SDK_TGZ=${DROP}/eclipse-SDK-${EVERSION}-linux-gtk-${ARCH}.tar.gz
