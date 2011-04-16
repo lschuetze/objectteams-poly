da86d55e94b5305da2c2919598b491c8ca7006a8
Stephan Herrmann <stephan@cs.tu-berlin.de>
2011-04-16 12:54:25 +0000

    Strengthen existing test and fix for Bug 335774 - [compiler] don't flag "bindingconventions" against import in superteam

--- org.eclipse.jdt.core/compiler/org/eclipse/jdt/internal/compiler/lookup/ClassScope.java
+++ org.eclipse.jdt.core/compiler/org/eclipse/jdt/internal/compiler/lookup/ClassScope.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2010 IBM Corporation and others.
+ * Copyright (c) 2000, 2011 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -63,6 +63,7 @@ import org.eclipse.objectteams.otdt.internal.core.compiler.statemachine.transfor
 import org.eclipse.objectteams.otdt.internal.core.compiler.statemachine.transformer.RoleSplitter;
 import org.eclipse.objectteams.otdt.internal.core.compiler.statemachine.transformer.StandardElementGenerator;
 import org.eclipse.objectteams.otdt.internal.core.compiler.util.AstGenerator;
+import org.eclipse.objectteams.otdt.internal.core.compiler.util.IAlienScopeTypeReference;
 import org.eclipse.objectteams.otdt.internal.core.compiler.util.Protections;
 import org.eclipse.objectteams.otdt.internal.core.compiler.util.Sorting;
 import org.eclipse.objectteams.otdt.internal.core.compiler.util.TSuperHelper;
@@ -2369,7 +2370,7 @@ public class ClassScope extends Scope {
 
 	private boolean isImportedType(TypeReference typeReference)
 	{
-		if (!(typeReference instanceof SingleTypeReference))
+		if (!(typeReference instanceof SingleTypeReference) || (typeReference instanceof IAlienScopeTypeReference))
 			return false;
 		if (typeReference instanceof ParameterizedSingleTypeReference) {
 			ParameterizedSingleTypeReference paramType = (ParameterizedSingleTypeReference) typeReference;
