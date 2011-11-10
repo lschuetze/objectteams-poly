ae64da7a4d423d87aa5502fdec0a9b73ab8b4d6a
Stephan Herrmann <stephan@cs.tu-berlin.de>
2011-11-10 09:47:39 +0000

    update jdt.core + tests to v_C19

---
+++ org.eclipse.jdt.core/compiler/org/eclipse/objectteams/otdt/internal/core/compiler/lookup/OTTagBits.java
@@ -0,0 +1,9 @@
+package org.eclipse.objectteams.otdt.internal.core.compiler.lookup;
+
+import org.eclipse.jdt.internal.compiler.ast.ASTNode;
+
+public interface OTTagBits {
+
+	long IsCopyOfParameterized = ASTNode.Bit1;			// for method bindings
+
+}
