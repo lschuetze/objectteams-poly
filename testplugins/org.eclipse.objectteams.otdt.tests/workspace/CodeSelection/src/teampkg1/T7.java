49686760cee19b8eea4b59219c8be4c30dc52d6f
Stephan Herrmann <sherrmann>
2012-02-05 21:04:45 +0100

    Add test resource for commit 7ce1dfcfcafa57052500d5791b03d5b1e8868d82

---
+++ testplugins/org.eclipse.objectteams.otdt.tests/workspace/CodeSelection/src/teampkg1/T7.java
@@ -0,0 +1,16 @@
+package teampkg1;
+
+import java.util.Set;
+
+public team class T7
+{
+    public class R1
+    {
+    	public void collect(Set<String> strings) {
+    		
+    	}
+    }
+    public void run(R1 r, Set<String> strings) {
+    	r.collect(strings);
+    }
+}
