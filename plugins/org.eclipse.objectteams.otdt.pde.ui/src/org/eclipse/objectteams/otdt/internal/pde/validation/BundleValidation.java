f9b6ac14ea349377df660947e16f0cefa17dd779
Stephan Herrmann <stephan.herrmann@berlin.de>
2013-07-30 01:29:57 +0200

    Bug 406518 - migrate OT/Equinox to the standard OSGi WeavingHook - new validation and quick fix for missing export of aspect package

--- plugins/org.eclipse.objectteams.otdt.pde.ui/src/org/eclipse/objectteams/otdt/internal/pde/validation/BundleValidation.java
+++ plugins/org.eclipse.objectteams.otdt.pde.ui/src/org/eclipse/objectteams/otdt/internal/pde/validation/BundleValidation.java
@@ -18,6 +18,9 @@ package org.eclipse.objectteams.otdt.internal.pde.validation;
 
 import static org.eclipse.objectteams.otequinox.Constants.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.eclipse.core.resources.IMarker;
 import org.eclipse.objectteams.otdt.internal.pde.ui.OTPDEUIMessages;
 import org.eclipse.objectteams.otequinox.ActivationKind;
@@ -30,12 +33,13 @@ import org.eclipse.pde.internal.core.text.bundle.BundleActivationPolicyHeader;
 import org.eclipse.pde.internal.core.text.bundle.BundleModel;
 import org.eclipse.pde.internal.ui.correction.AbstractManifestMarkerResolution;
 import org.eclipse.pde.internal.ui.correction.AbstractPDEMarkerResolution;
+import org.eclipse.pde.internal.ui.correction.AddExportPackageMarkerResolution;
 import org.eclipse.ui.IMarkerResolution;
 import org.osgi.framework.Constants;
 import org.eclipse.osgi.service.resolver.BundleDescription;
 import org.eclipse.osgi.service.resolver.State;
+import org.eclipse.osgi.util.ManifestElement;
 import org.eclipse.osgi.util.NLS;
-
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
 
@@ -59,6 +63,8 @@ public team class BundleValidation
 {
 	/** Constant for a problem that can be resolved by adding an activation policy to the manifest. */
 	static final int ADD_ACTIVATION_POLICY = 0x1801; // must not overlap with any constant in org.eclipse.pde.internal.core.builders.PDEMarkerFactory. 
+	/** Constant for a problem that can be resolved by adding an activation policy to the manifest. */
+	static final int ADD_PACKAGE_EXPORT = 0x1802; // must not overlap with any constant in org.eclipse.pde.internal.core.builders.PDEMarkerFactory. 
 	
 	ThreadLocal<BundleCheckingContext> bundleContext = new ThreadLocal<BundleCheckingContext>();
 	
@@ -72,6 +78,7 @@ public team class BundleValidation
 		// flags set during validation of one bundle:
 		protected boolean isAspectBundle = false;
 		protected boolean hasTeamActivation = false;
+		protected List<String> aspectPackages = new ArrayList<String>();
 
 		@SuppressWarnings("decapsulation")
 		spanContext <- replace validateFiles;
@@ -131,7 +138,16 @@ public team class BundleValidation
 				// check the teams for activation ALL_THREADS or THREAD:
 				NodeList teamNodes = element.getElementsByTagName(TEAM);
 				for (int t=0; t<teamNodes.getLength(); t++) {
-					Object activation = ((Element)teamNodes.item(t)).getAttribute(ACTIVATION);
+					// record aspect packages:
+					Element teamNode = (Element)teamNodes.item(t);
+					Object teamClass = teamNode.getAttribute(CLASS);
+					if (teamClass instanceof String) {
+						String teamName = (String) teamClass;
+						int lastDot = teamName.lastIndexOf('.');
+						context.aspectPackages.add(teamName.substring(0, lastDot));
+					}
+					// team activation?
+					Object activation = teamNode.getAttribute(ACTIVATION);
 					if (ActivationKind.ALL_THREADS.toString().equals(activation)) {
 						context.hasTeamActivation = true;
 						break;
@@ -160,8 +176,11 @@ public team class BundleValidation
 			base when (BundleValidation.this.bundleContext.get().isAspectBundle)
 	{			
 		@SuppressWarnings("decapsulation")
+		void addMarkerAttribute(IMarker marker, String attr, String val)
+			-> void addMarkerAttribute(IMarker marker, String attr, String val);
+		@SuppressWarnings("decapsulation")
 		IHeader getHeader(String key) -> IHeader getHeader(String key);
-		void report(String message, int line, int severity, int resolution, String category) 
+		IMarker report(String message, int line, int severity, int resolution, String category) 
 			-> IMarker report(String message, int line, int severity, int resolution, String category);
 		
 		void validateBundleActivatorPolicy() <- after void validateBundleActivatorPolicy();
@@ -181,9 +200,42 @@ public team class BundleValidation
 				   hasTeamActivation ? CompilerFlags.ERROR : CompilerFlags.WARNING, 	// only severe if relevant team activation is requested. 
 				   ADD_ACTIVATION_POLICY, 
 				   PDEMarkerFactory.CAT_FATAL);
-		}		
+		}
+
+		void validateExportPackages() <- after void validateExportPackages();
+
+		void validateExportPackages() {
+			List<String> needingExport = bundleContext.get().aspectPackages;
+			if (needingExport.isEmpty()) return;
+			IHeader header = getHeader(Constants.EXPORT_PACKAGE);
+			if (header != null) {
+				ManifestElement[] elements = header.getElements();
+				for (int i = 0; i < elements.length; i++)
+					needingExport.remove(elements[i].getValue());
+			}
+			for (String unmatched : needingExport) {
+				IMarker marker = report(NLS.bind(OTPDEUIMessages.Validation_MissingAspectPackageExport_error, unmatched), 
+						   1, 
+						   CompilerFlags.ERROR, 	// can reduce severity when we have the option to add the export at runtime 
+						   ADD_PACKAGE_EXPORT, 
+						   PDEMarkerFactory.CAT_FATAL);
+				addMarkerAttribute(marker, "package", unmatched); //$NON-NLS-1$
+
+				IHeader aspectBundleName = getHeader(Constants.BUNDLE_SYMBOLICNAME);
+				if (aspectBundleName != null && aspectBundleName.getValue() != null) {
+					String bundleSymbolicName = aspectBundleName.getValue();
+					int semi = bundleSymbolicName.indexOf(';');
+					if (semi != -1)
+						bundleSymbolicName = bundleSymbolicName.substring(0, semi); // strip of attributes/directives like ;singleton:=true
+					addMarkerAttribute(marker, "export", unmatched+";ot-aspect-host=\""+bundleSymbolicName+"\""); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+				}
+				else {
+					addMarkerAttribute(marker, "export", unmatched); //$NON-NLS-1$
+				}
+			}
+		}
 	}
-	
+
 	/** Unbound role: simple rewriting of the manifest to add or correct an activation policy header. */
 	protected class SetActivationPolicyResolution extends AbstractManifestMarkerResolution 
 	{	
@@ -206,6 +258,25 @@ public team class BundleValidation
 		}
 	}
 	
+	/** Unbound role: simple rewriting of the manifest to add an Export-Package header. */
+	protected class ExportAspectPackageResolution extends AddExportPackageMarkerResolution {
+		String packageName;
+		String export; // extended version with ot-aspect-host attribute
+		public ExportAspectPackageResolution(IMarker marker) {
+			super(AbstractPDEMarkerResolution.CREATE_TYPE, marker.getAttribute("export", null)); //$NON-NLS-1$
+			this.packageName = marker.getAttribute("package", null); //$NON-NLS-1$
+			this.export = marker.getAttribute("export", null); //$NON-NLS-1$
+		}
+		@Override
+		public String getLabel() {
+			return NLS.bind(OTPDEUIMessages.Resolution_AddAspectPackageExport_label, packageName);
+		}
+		@Override
+		public String getDescription() {
+			return NLS.bind(OTPDEUIMessages.Resolution_AddAspectPackageExport_description, packageName, export);
+		}
+	}
+	
 	/**
 	 * Advise the base class for handling missing/incorrect activation policy 
 	 * (code {@link BundleValidation#ADD_ACTIVATION_POLICY}).
@@ -217,10 +288,12 @@ public team class BundleValidation
 		callin IMarkerResolution[] getResolutions(IMarker marker) {
 			IMarkerResolution[] result = base.getResolutions(marker);
 			if (result.length == 0) {
-				int problemID = marker.getAttribute("id", PDEMarkerFactory.NO_RESOLUTION); //$NON-NLS-1$
+				int problemID = marker.getAttribute(PDEMarkerFactory.PROBLEM_ID, PDEMarkerFactory.NO_RESOLUTION);
 				switch (problemID) {
 					case BundleValidation.ADD_ACTIVATION_POLICY :
 						return new IMarkerResolution[] {new SetActivationPolicyResolution(AbstractPDEMarkerResolution.CREATE_TYPE)};
+					case BundleValidation.ADD_PACKAGE_EXPORT :
+						return new IMarkerResolution[] {new ExportAspectPackageResolution(marker) };
 				}
 			}
 			return result;
