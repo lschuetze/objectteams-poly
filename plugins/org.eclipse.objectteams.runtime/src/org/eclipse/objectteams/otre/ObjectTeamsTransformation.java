d7011ef9b23a10e56f86e3dc517e7d9c609c8968
Stephan Herrmann <stephan.herrmann@berlin.de>
2013-07-30 18:15:07 +0200

    Bug 406518 - migrate OT/Equinox to the standard OSGi WeavingHook - debug output for errors in org.eclipse.objectteams.otdt.test.builder

--- plugins/org.eclipse.objectteams.runtime/src/org/eclipse/objectteams/otre/ObjectTeamsTransformation.java
+++ plugins/org.eclipse.objectteams.runtime/src/org/eclipse/objectteams/otre/ObjectTeamsTransformation.java
@@ -34,7 +34,6 @@ import org.eclipse.objectteams.otre.util.AnnotationHelper;
 import org.eclipse.objectteams.otre.util.AttributeReadingGuard;
 import org.eclipse.objectteams.otre.util.CallinBindingManager;
 import org.eclipse.objectteams.otre.util.RoleBaseBinding;
-
 import org.apache.bcel.Constants;
 import org.apache.bcel.classfile.Attribute;
 import org.apache.bcel.classfile.Constant;
@@ -572,14 +571,17 @@ public abstract class ObjectTeamsTransformation
     	List<String> classesToLoad;
     	synchronized (guard) {
     		if (!guard.iAmTheFirst(class_name)) {
-    			if (isScanOnly) {
+    			if (isScanOnly && class_name.equals("org.eclipse.objectteams.otdt.test.builder.OTTestingEnvironment")) {
     				System.err.println("I'm not the first "+this);
     				System.err.println(" guard ="+guard);
     				System.err.println(" loader="+this.loader);
     				System.err.println(" class ="+class_name);
+    				new Exception("I'm not the first "+this).printStackTrace();
     			}
 				return;
 			}
+    		if (class_name.equals("org.eclipse.objectteams.otdt.test.builder.OTTestingEnvironment"))
+    			new Exception("I AM the first "+this).printStackTrace();
 			if (AttributeReadingGuard.isFirstLoadedClass())
 				addTeamInitializations = true;
 			// scan for attributes here, because this transformer is applied first:
