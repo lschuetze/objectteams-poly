9b5f216eb1069dc8f5211bf81bf5e2fb46bebbe1
Stephan Herrmann <stephan.herrmann@berlin.de>
2013-07-30 18:59:59 +0200

    Bug 406518 - migrate OT/Equinox to the standard OSGi WeavingHook - test adjust. and new test for new validation (export aspect package)

---
+++ testplugins/org.eclipse.objectteams.otdt.test.builder/workspace/AspectExportAspect/src/aea/Team18.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package aea;
+
+import base aeb.api.PublicBase;
+
+/**
+ * @author stephan
+ *
+ */
+public team class Team18 {
+	protected class RoleOfPublic playedBy PublicBase {
+		
+	}
+}
---
+++ testplugins/org.eclipse.objectteams.otdt.test.builder/workspace/AspectExportBase/src/aeb/api/PublicBase.java
@@ -0,0 +1,5 @@
+package aeb.api;
+
+public class PublicBase {
+
+}
