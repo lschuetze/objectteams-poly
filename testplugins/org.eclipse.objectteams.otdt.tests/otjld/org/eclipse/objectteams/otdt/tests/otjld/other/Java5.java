31c504bb35c9ef50b81d5491ceab32f70a2fe11d
Stephan Herrmann <stephan.herrmann@berlin.de>
2014-04-24 16:35:42 +0200

    Disable one test in Java5 feeding bytes into BCEL which it cannot grok

--- testplugins/org.eclipse.objectteams.otdt.tests/otjld/org/eclipse/objectteams/otdt/tests/otjld/other/Java5.java
+++ testplugins/org.eclipse.objectteams.otdt.tests/otjld/org/eclipse/objectteams/otdt/tests/otjld/other/Java5.java
@@ -3914,7 +3914,11 @@ public class Java5 extends AbstractOTJLDTest {
     // a role method has a custom annotation, so should its tsub  -  testing at runtime via reflection
     // A.1.17-otjld-copyinheritance-for-annotation-4
     public void testA117_copyinheritanceForAnnotation4() {
-       
+       if (this.weavingScheme == WeavingScheme.OTRE && this.complianceLevel >= ClassFileConstants.JDK1_8) {
+    	   // bytecode at 1.8 contains a method handle, cannot be read by BCEL
+    	   System.err.println("Cannot execute a test at 1.8+ against OTRE");
+    	   return;
+       }
        runConformTest(
             new String[] {
 		"TeamA117cfa4_2.java",
