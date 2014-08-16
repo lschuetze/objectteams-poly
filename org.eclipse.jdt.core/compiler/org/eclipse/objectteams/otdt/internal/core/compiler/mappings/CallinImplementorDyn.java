b8b0db6168b85fe5a8bd37bebba23d208c53b310
Stephan Herrmann <stephan.herrmann@berlin.de>
2014-08-16 11:38:57 +0200

    Bug 436759 [otdre] Finish implementation of OTDRE - fix TeamNesting.test1129_nestedCallin2

--- org.eclipse.jdt.core/compiler/org/eclipse/objectteams/otdt/internal/core/compiler/mappings/CallinImplementorDyn.java
+++ org.eclipse.jdt.core/compiler/org/eclipse/objectteams/otdt/internal/core/compiler/mappings/CallinImplementorDyn.java
@@ -617,7 +617,7 @@ public class CallinImplementorDyn extends MethodMappingImplementor {
 								//   who is responsible for lowering: the team or the current role?
 								Expression lowerReceiver = (isRoleOfCurrentRole(roleType, returnTypes[0]))
 										? gen.singleNameReference(roleVar)
-										: gen.thisReference();
+										: genTeamThis(gen, returnTypes[0]);
 								result = new Lowering().lowerExpression(methodDecl.scope, result, returnTypes[0], returnTypes[1],
 										lowerReceiver, true/*needNullCheck*/, true/*delayedResolve*/);
 							}
@@ -906,7 +906,7 @@ public class CallinImplementorDyn extends MethodMappingImplementor {
 										gen.castExpression(gen.singleNameReference(IOTConstants.BASE), gen.typeReference(currentRole.baseclass()), CastExpression.RAW),
 										currentRole.baseclass(), currentRole, false)
 								// TODO: might want to extend the signature of callNext to pass the current role to avoid this lifting?
-					: gen.thisReference();
+					: genTeamThis(gen, returnTypes[0]);
 				result = Lifting.liftCall(mapping.scope,
 										  liftReceiver,
 										  gen.castExpression(result,
@@ -933,6 +933,16 @@ public class CallinImplementorDyn extends MethodMappingImplementor {
 		};
 		decl.hasParsedStatements = true;
 		AstEdit.addMethod(teamDecl, decl);
+	}
+
+	Reference genTeamThis(AstGenerator gen, TypeBinding type) {
+		TypeBinding leaf = type.leafComponentType();
+		if (leaf instanceof ReferenceBinding) {
+			ReferenceBinding teamBinding = ((ReferenceBinding) leaf).enclosingType();
+			if (teamBinding != null)
+				return gen.qualifiedThisReference(teamBinding);
+		}
+		return gen.thisReference();
 	}
 
 	TypeBinding[] getReturnTypes(CallinMappingDeclaration mapping, int i) {
