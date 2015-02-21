lschuetze/objectteams-poly

commit cfc352b34fa06e82001f50e0e1d44fa5dc05a141

Bug 459952 - New formatter for OT/J after bug 303519 - hard coded formatting for OT/J elements - supported by new kind for ASTParser (param map). - incl. fix in ASTConverter re base-calls

org.eclipse.jdt.core/compiler/org/eclipse/objectteams/otdt/internal/core/compiler/ast/BaseCallMessageSend.java
@@ -1,7 +1,7 @@
 /**********************************************************************
  * This file is part of "Object Teams Development Tooling"-Software
  *
- * Copyright 2004, 2006 Fraunhofer Gesellschaft, Munich, Germany,
+ * Copyright 2004, 2015 Fraunhofer Gesellschaft, Munich, Germany,
  * for its Fraunhofer Institute for Computer Architecture and Software
  * Technology (FIRST), Berlin, Germany and Technical University Berlin,
  * Germany.
@@ -10,7 +10,6 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * $Id: BaseCallMessageSend.java 23401 2010-02-02 23:56:05Z stephan $
  *
  * Please visit http://www.eclipse.org/objectteams for updates and contact.
  *
@@ -71,10 +70,7 @@ import org.eclipse.objectteams.otdt.internal.core.compiler.util.AstGenerator;
  *
  * What: create a method binding faking the base call surrogate to be created by the OTRE
  *
- * TODO(SH): pretty printing, mainly for error reporting! (hide enhancements!)
- *
  * @author gis
- * @version $Id: BaseCallMessageSend.java 23401 2010-02-02 23:56:05Z stephan $
  */
 public class BaseCallMessageSend extends AbstractExpressionWrapper
 {
@@ -422,7 +418,8 @@ public class BaseCallMessageSend extends AbstractExpressionWrapper
 				this._sendOrig.selector = this.sourceSelector;
 				argsSave = this._sendOrig.arguments;
 				boolean isEnhanced = (this._sendOrig.bits & ASTNode.HasBeenTransformed) != 0;
-				this._sendOrig.arguments = MethodSignatureEnhancer.retrenchBasecallArguments(argsSave, isEnhanced, this._weavingScheme);
+				if (isEnhanced)
+					this._sendOrig.arguments = MethodSignatureEnhancer.retrenchBasecallArguments(argsSave, true, this._weavingScheme);
 				return this._sendOrig.printExpression(indent, output);
 			}
 		} finally {

org.eclipse.jdt.core/META-INF/MANIFEST.MF
@@ -53,7 +53,7 @@ Export-Package: org.eclipse.jdt.core,
  org.eclipse.jdt.internal.eval;x-internal:=true,
  org.eclipse.jdt.internal.formatter;x-internal:=true,
  org.eclipse.jdt.internal.formatter.linewrap;x-internal:=true,
- org.eclipse.jdt.internal.formatter.old;x-internal:=true
+ org.eclipse.jdt.internal.formatter.old;x-internal:=true,
  org.eclipse.objectteams.otdt.core,
  org.eclipse.objectteams.otdt.core.compiler,
  org.eclipse.objectteams.otdt.core.exceptions,

org.eclipse.jdt.core/dom/org/eclipse/jdt/core/dom/ASTConverter.java
@@ -6518,7 +6518,9 @@ public BaseConstructorInvocation convert(
 		if (arguments != null) {
 			int argumentsLength = arguments.length;
 			// FIXME(SH): static?
-			int startIdx = MethodSignatureEnhancer.getEnhancingArgLen(this.weavingScheme)+1; // +1: skip 'isSuperAccess'
+			int startIdx = 0;
+			if ((msgSend.bits & org.eclipse.jdt.internal.compiler.ast.ASTNode.HasBeenTransformed) != 0)
+				startIdx = MethodSignatureEnhancer.getEnhancingArgLen(this.weavingScheme)+1; // +1: skip 'isSuperAccess'
 			for (int idx = startIdx; idx < argumentsLength; idx++) {
 				Expression argExpr = convert(arguments[idx]);
 				if (this.resolveBindings) {

org.eclipse.jdt.core/dom/org/eclipse/jdt/core/dom/ASTParser.java
@@ -120,6 +120,14 @@ public class ASTParser {
 	 */
 	public static final int K_COMPILATION_UNIT = 0x08;
 
+//{ObjectTeams:
+	/**
+	 * Kind constant used to request that the source be parsed
+	 * as a parameter mapping.
+	 */
+	public static final int K_PARAMETER_MAPPING = 0x10;	
+// SH}
+
 	/**
 	 * Creates a new object for creating a Java abstract syntax tree
      * (AST) following the specified set of API rules.
@@ -566,6 +574,9 @@ public class ASTParser {
 	    if ((kind != K_COMPILATION_UNIT)
 		    && (kind != K_CLASS_BODY_DECLARATIONS)
 		    && (kind != K_EXPRESSION)
+//{ObjectTeams:
+		    && (kind != K_PARAMETER_MAPPING)
+// SH}
 		    && (kind != K_STATEMENTS)) {
 	    	throw new IllegalArgumentException();
 	    }
@@ -1069,6 +1080,9 @@ public class ASTParser {
 		switch(this.astKind) {
 			case K_CLASS_BODY_DECLARATIONS :
 			case K_EXPRESSION :
+//{ObjectTeams:
+			case K_PARAMETER_MAPPING:
+// SH}
 			case K_STATEMENTS :
 				if (this.rawSource == null) {
 					if (this.typeRoot != null) {
@@ -1386,7 +1400,15 @@ public class ASTParser {
 				ast.setOriginalModificationCount(ast.modificationCount());
 				return block;
 			case K_EXPRESSION :
+//{ObjectTeams:
+			case K_PARAMETER_MAPPING:
+/* orig:
 				org.eclipse.jdt.internal.compiler.ast.Expression expression = codeSnippetParsingUtil.parseExpression(this.rawSource, this.sourceOffset, this.sourceLength, this.compilerOptions, true);
+  :giro */
+				org.eclipse.jdt.internal.compiler.ast.Expression expression = this.astKind == K_EXPRESSION
+							? codeSnippetParsingUtil.parseExpression(this.rawSource, this.sourceOffset, this.sourceLength, this.compilerOptions, true)
+							: codeSnippetParsingUtil.parseParameterMapping(this.rawSource, this.sourceOffset, this.sourceLength, this.compilerOptions, true);
+// SH}
 				recordedParsingInformation = codeSnippetParsingUtil.recordedParsingInformation;
 				comments = recordedParsingInformation.commentPositions;
 				if (comments != null) {

org.eclipse.jdt.core/formatter/org/eclipse/jdt/internal/formatter/DefaultCodeFormatter.java
@@ -297,6 +297,10 @@ public class DefaultCodeFormatter extends CodeFormatter {
 				return parseSourceCode(parser, ASTParser.K_STATEMENTS, false);
 			case K_EXPRESSION:
 				return parseSourceCode(parser, ASTParser.K_EXPRESSION, false);
+//{ObjectTeams: own kind:
+			case K_PARAMETER_MAPPING:
+				return parseSourceCode(parser, ASTParser.K_PARAMETER_MAPPING, false);
+// SH}
 			case K_UNKNOWN:
 				int[] parserModes = { ASTParser.K_COMPILATION_UNIT, ASTParser.K_EXPRESSION,
 						ASTParser.K_CLASS_BODY_DECLARATIONS, ASTParser.K_STATEMENTS };

org.eclipse.jdt.core/formatter/org/eclipse/jdt/internal/formatter/LineBreaksPreparator.java
@@ -16,9 +16,11 @@ import static org.eclipse.jdt.internal.compiler.parser.TerminalTokens.TokenNameC
 import static org.eclipse.jdt.internal.compiler.parser.TerminalTokens.TokenNameLBRACE;
 import static org.eclipse.jdt.internal.compiler.parser.TerminalTokens.TokenNameRBRACE;
 import static org.eclipse.jdt.internal.compiler.parser.TerminalTokens.TokenNameSEMICOLON;
+import static org.eclipse.jdt.internal.compiler.parser.TerminalTokens.TokenNamebase;
 import static org.eclipse.jdt.internal.compiler.parser.TerminalTokens.TokenNameelse;
 import static org.eclipse.jdt.internal.compiler.parser.TerminalTokens.TokenNamefinally;
 import static org.eclipse.jdt.internal.compiler.parser.TerminalTokens.TokenNamepackage;
+import static org.eclipse.jdt.internal.compiler.parser.TerminalTokens.TokenNamewhen;
 import static org.eclipse.jdt.internal.compiler.parser.TerminalTokens.TokenNamewhile;
 
 import java.util.List;
@@ -34,6 +36,8 @@ import org.eclipse.jdt.core.dom.ArrayInitializer;
 import org.eclipse.jdt.core.dom.Block;
 import org.eclipse.jdt.core.dom.BodyDeclaration;
 import org.eclipse.jdt.core.dom.BreakStatement;
+import org.eclipse.jdt.core.dom.CallinMappingDeclaration;
+import org.eclipse.jdt.core.dom.CalloutMappingDeclaration;
 import org.eclipse.jdt.core.dom.CatchClause;
 import org.eclipse.jdt.core.dom.CompilationUnit;
 import org.eclipse.jdt.core.dom.DoStatement;
@@ -43,6 +47,7 @@ import org.eclipse.jdt.core.dom.EnumConstantDeclaration;
 import org.eclipse.jdt.core.dom.EnumDeclaration;
 import org.eclipse.jdt.core.dom.FieldDeclaration;
 import org.eclipse.jdt.core.dom.ForStatement;
+import org.eclipse.jdt.core.dom.GuardPredicateDeclaration;
 import org.eclipse.jdt.core.dom.IfStatement;
 import org.eclipse.jdt.core.dom.ImportDeclaration;
 import org.eclipse.jdt.core.dom.Initializer;
@@ -53,6 +58,7 @@ import org.eclipse.jdt.core.dom.MethodDeclaration;
 import org.eclipse.jdt.core.dom.Modifier;
 import org.eclipse.jdt.core.dom.NormalAnnotation;
 import org.eclipse.jdt.core.dom.PackageDeclaration;
+import org.eclipse.jdt.core.dom.RoleTypeDeclaration;
 import org.eclipse.jdt.core.dom.SingleMemberAnnotation;
 import org.eclipse.jdt.core.dom.SingleVariableDeclaration;
 import org.eclipse.jdt.core.dom.Statement;
@@ -63,6 +69,7 @@ import org.eclipse.jdt.core.dom.TypeDeclaration;
 import org.eclipse.jdt.core.dom.VariableDeclarationExpression;
 import org.eclipse.jdt.core.dom.VariableDeclarationStatement;
 import org.eclipse.jdt.core.dom.WhileStatement;
+import org.eclipse.jdt.core.dom.WithinStatement;
 import org.eclipse.jdt.core.formatter.DefaultCodeFormatterConstants;
 
 public class LineBreaksPreparator extends ASTVisitor {
@@ -118,6 +125,15 @@ public class LineBreaksPreparator extends ASTVisitor {
 	}
 
 	public boolean visit(TypeDeclaration node) {
+//{ObjectTeams:
+		GuardPredicateDeclaration predicate = node.getGuardPredicate();
+		if (predicate != null) {
+			this.tm.firstTokenIn(node.getGuardPredicate(), predicate.isBase() ? TokenNamebase : TokenNamewhen).breakBefore();
+			indent(node.getGuardPredicate());
+			indent(node.getGuardPredicate());
+			this.tm.firstTokenAfter(node.getGuardPredicate(), TokenNameLBRACE).breakBefore();
+		}			
+// SH}
 		handleBodyDeclarations(node.bodyDeclarations());
 
 		if (node.getName().getStartPosition() == -1)
@@ -516,6 +532,46 @@ public class LineBreaksPreparator extends ASTVisitor {
 			breakLineBefore(node);
 		return true;
 	}
+
+//{ObjectTeams: more visits:
+	@Override
+	public boolean visit(RoleTypeDeclaration node) {
+		return visit((TypeDeclaration)node);
+	}
+	@Override
+	public boolean visit(CalloutMappingDeclaration node) {
+		if (node.hasParameterMapping()) {
+			for (Object pMap : node.getParameterMappings())
+				breakLineBefore((ASTNode) pMap);
+			String bracePosition = this.options.brace_position_for_block;
+			handleBracedCode(node, null, bracePosition, this.options.indent_statements_compare_to_block,
+					this.options.insert_new_line_in_empty_block);
+		}
+		return true;
+	}
+	@Override
+	public boolean visit(CallinMappingDeclaration node) {
+		GuardPredicateDeclaration predicate = node.getGuardPredicate();
+		if (predicate != null) {
+			this.tm.firstTokenIn(node.getGuardPredicate(), predicate.isBase() ? TokenNamebase : TokenNamewhen).breakBefore();
+			indent(node.getGuardPredicate());
+		}
+
+		if (node.hasParameterMapping()) {
+			for (Object pMap : node.getParameterMappings())
+				breakLineBefore((ASTNode) pMap);
+			String bracePosition = this.options.brace_position_for_block;
+			handleBracedCode(node, null, bracePosition, this.options.indent_statements_compare_to_block,
+					this.options.insert_new_line_in_empty_block);
+		}
+		return true;
+	}
+	@Override
+	public boolean visit(WithinStatement node) { // like while
+		handleLoopBody(node.getBody());
+		return true;
+	}
+// SH}
 
 	private void breakLineBefore(ASTNode node) {
 		this.tm.firstTokenIn(node, -1).breakBefore();

org.eclipse.jdt.core/formatter/org/eclipse/jdt/internal/formatter/SpacePreparator.java
@@ -12,6 +12,7 @@ package org.eclipse.jdt.internal.formatter;
 
 import static org.eclipse.jdt.internal.compiler.parser.TerminalTokens.*;
 
+import java.util.Collections;
 import java.util.List;
 
 import org.eclipse.jdt.core.dom.ASTNode;
@@ -26,7 +27,10 @@ import org.eclipse.jdt.core.dom.ArrayInitializer;
 import org.eclipse.jdt.core.dom.ArrayType;
 import org.eclipse.jdt.core.dom.AssertStatement;
 import org.eclipse.jdt.core.dom.Assignment;
+import org.eclipse.jdt.core.dom.BaseCallMessageSend;
 import org.eclipse.jdt.core.dom.Block;
+import org.eclipse.jdt.core.dom.CallinMappingDeclaration;
+import org.eclipse.jdt.core.dom.CalloutMappingDeclaration;
 import org.eclipse.jdt.core.dom.CastExpression;
 import org.eclipse.jdt.core.dom.CatchClause;
 import org.eclipse.jdt.core.dom.ClassInstanceCreation;
@@ -43,6 +47,7 @@ import org.eclipse.jdt.core.dom.ExpressionMethodReference;
 import org.eclipse.jdt.core.dom.ExpressionStatement;
 import org.eclipse.jdt.core.dom.FieldDeclaration;
 import org.eclipse.jdt.core.dom.ForStatement;
+import org.eclipse.jdt.core.dom.GuardPredicateDeclaration;
 import org.eclipse.jdt.core.dom.IfStatement;
 import org.eclipse.jdt.core.dom.InfixExpression;
 import org.eclipse.jdt.core.dom.InstanceofExpression;
@@ -54,10 +59,12 @@ import org.eclipse.jdt.core.dom.MemberValuePair;
 import org.eclipse.jdt.core.dom.MethodDeclaration;
 import org.eclipse.jdt.core.dom.MethodInvocation;
 import org.eclipse.jdt.core.dom.NormalAnnotation;
+import org.eclipse.jdt.core.dom.ParameterMapping;
 import org.eclipse.jdt.core.dom.ParameterizedType;
 import org.eclipse.jdt.core.dom.ParenthesizedExpression;
 import org.eclipse.jdt.core.dom.PostfixExpression;
 import org.eclipse.jdt.core.dom.PrefixExpression;
+import org.eclipse.jdt.core.dom.RoleTypeDeclaration;
 import org.eclipse.jdt.core.dom.PrefixExpression.Operator;
 import org.eclipse.jdt.core.dom.ReturnStatement;
 import org.eclipse.jdt.core.dom.SingleMemberAnnotation;
@@ -82,6 +89,7 @@ import org.eclipse.jdt.core.dom.VariableDeclarationFragment;
 import org.eclipse.jdt.core.dom.VariableDeclarationStatement;
 import org.eclipse.jdt.core.dom.WhileStatement;
 import org.eclipse.jdt.core.dom.WildcardType;
+import org.eclipse.jdt.core.dom.WithinStatement;
 import org.eclipse.jdt.internal.compiler.parser.ScannerHelper;
 
 public class SpacePreparator extends ASTVisitor {
@@ -873,6 +881,62 @@ public class SpacePreparator extends ASTVisitor {
 		handleTypeArguments(node.typeArguments());
 		return true;
 	}
+
+//{ObjectTeams: more visits:
+	@Override
+	public boolean visit(RoleTypeDeclaration node) {
+		return visit((TypeDeclaration)node);
+	}
+	@Override
+	public boolean visit(BaseCallMessageSend node) {
+		// see MethodInvocation (sauf type arguments).
+		handleInvocation(node, node.getName(), Collections.<Type>emptyList());
+		handleCommas(node.getArguments(), this.options.insert_space_before_comma_in_method_invocation_arguments,
+				this.options.insert_space_after_comma_in_method_invocation_arguments);
+		return true;
+	}
+	@Override
+	public boolean visit(CalloutMappingDeclaration node) {
+		handleTokenAfter(node.getRoleMappingElement(), TokenNameBINDOUT, true, true);
+		if (node.hasParameterMapping())
+			handleTokenAfter(node.getBaseMappingElement(), TokenNameLBRACE, true, true);
+		return true;
+	}
+	@Override
+	public boolean visit(CallinMappingDeclaration node) {
+		handleTokenAfter(node.getRoleMappingElement(), TokenNameBINDIN, true, true);
+		List<ASTNode> baseMappingElements = node.getBaseMappingElements();
+		handleCommas(baseMappingElements, false, true);
+		ASTNode lastBase = baseMappingElements.get(baseMappingElements.size()-1);
+		GuardPredicateDeclaration predicate = node.getGuardPredicate();
+		if (predicate != null)
+			handleTokenAfter(lastBase, predicate.isBase() ? TokenNamebase : TokenNamewhen, true, true);
+		if (node.hasParameterMapping())
+			handleTokenAfter(lastBase, TokenNamewith, true, true);
+		return true;
+	}
+	@Override
+	public boolean visit(ParameterMapping node) {
+		if (node.isBindIN())
+			handleTokenAfter(node.getIdentifier(), TokenNameBINDIN, true, true);
+		else
+			handleTokenBefore(node.getIdentifier(), TokenNameBINDOUT, true, true);
+		return true;
+	}
+	@Override
+	public boolean visit(GuardPredicateDeclaration node) {
+		handleToken(node, TokenNamewhen, true, true);
+		return true;
+	}
+	@Override
+	public boolean visit(WithinStatement node) { // like while
+		handleToken(node, TokenNameLPAREN, this.options.insert_space_before_opening_paren_in_while,
+				this.options.insert_space_after_opening_paren_in_while);
+		handleTokenBefore(node.getBody(), TokenNameRPAREN, this.options.insert_space_before_closing_paren_in_while,
+				false);
+		return true;
+	}
+// SH}
 
 	private void handleTypeArguments(List<Type> typeArguments) {
 		if (typeArguments.isEmpty())

testplugins/org.eclipse.objectteams.otdt.tests/formatter/org/eclipse/objectteams/otdt/core/tests/formatter/FormatterTests.java
@@ -1,16 +1,15 @@
 /**********************************************************************
  * This file is part of "Object Teams Development Tooling"-Software
  * 
- * Copyright 2004, 2010 Fraunhofer Gesellschaft, Munich, Germany,
+ * Copyright 2004, 2015 Fraunhofer Gesellschaft, Munich, Germany,
  * for its Fraunhofer Institute and Computer Architecture and Software
  * Technology (FIRST), Berlin, Germany and Technical University Berlin,
- * Germany.
+ * Germany, and others.
  * 
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * $Id: FormatterTests.java 23494 2010-02-05 23:06:44Z stephan $
  * 
  * Please visit http://www.eclipse.org/objectteams for updates and contact.
  * 
@@ -47,7 +46,6 @@ import org.eclipse.text.edits.TextEdit;
  * 
  * @author stephan
  */
-@SuppressWarnings("restriction")
 public class FormatterTests extends AbstractJavaModelTests {
 		
 	public static final int UNKNOWN_KIND = 0;
@@ -59,6 +57,7 @@ public class FormatterTests extends AbstractJavaModelTests {
 	
 	static {
 //		TESTS_NUMBERS = new int[] { 620 } ;
+//		TESTS_NAMES = new String{ "test005" };
 	}
 	public static Test suite() {
 		return buildModelTestSuite(FormatterTests.class);

commit 3b0f895081662eb3b52893965b1877c679268e43

Fix regression caused by commit cfc352b34fa06e82001f50e0e1d44fa5dc05a141 - error message uses BaseCallMessageSend.toString()!

org.eclipse.jdt.core/compiler/org/eclipse/objectteams/otdt/internal/core/compiler/ast/BaseCallMessageSend.java
@@ -417,9 +417,8 @@ public class BaseCallMessageSend extends AbstractExpressionWrapper
 				selectorSave = this._sendOrig.selector;
 				this._sendOrig.selector = this.sourceSelector;
 				argsSave = this._sendOrig.arguments;
-				boolean isEnhanced = (this._sendOrig.bits & ASTNode.HasBeenTransformed) != 0;
-				if (isEnhanced)
-					this._sendOrig.arguments = MethodSignatureEnhancer.retrenchBasecallArguments(argsSave, true, this._weavingScheme);
+				if (hasBeenTransformed(this)) // signals presence of superAccess flag
+					this._sendOrig.arguments = MethodSignatureEnhancer.retrenchBasecallArguments(argsSave, hasBeenTransformed(this._sendOrig), this._weavingScheme);
 				return this._sendOrig.printExpression(indent, output);
 			}
 		} finally {
@@ -429,5 +428,8 @@ public class BaseCallMessageSend extends AbstractExpressionWrapper
 				this._sendOrig.arguments = argsSave;
 		}
 		return super.printExpression(indent, output);
+	}
+	private static boolean hasBeenTransformed(Expression expr) {
+		return (expr.bits & ASTNode.HasBeenTransformed) != 0;
 	}
 }

org.eclipse.jdt.core/compiler/org/eclipse/objectteams/otdt/internal/core/compiler/statemachine/transformer/TransformStatementsVisitor.java
@@ -146,6 +146,7 @@ public class TransformStatementsVisitor
     @Override
     public boolean visit(BaseCallMessageSend messageSend, BlockScope scope) {
     	messageSend.prepareSuperAccess(this.weavingScheme);
+		messageSend.bits |= ASTNode.HasBeenTransformed; // only the outer has been transformed so far.
     	return true;
     }
     
@@ -202,7 +203,7 @@ public class TransformStatementsVisitor
 	    		}
     		}
 			messageSend.arguments = MethodSignatureEnhancer.enhanceArguments(args, messageSend.sourceEnd+1, this.weavingScheme);
-			messageSend.bits |= ASTNode.HasBeenTransformed;
+			messageSend.bits |= ASTNode.HasBeenTransformed; // mark only when args have really been enhanced
     	}
     	return true;
     }