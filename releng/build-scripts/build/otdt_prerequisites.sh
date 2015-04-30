9e2599e6ab4c3c371b2d94c47e2318f48ff33ea4
Stephan Herrmann <stephan.herrmann@berlin.de>
2015-04-30 15:03:12 +0200

    Build against I20150429-2000 (M7 candidate).

--- releng/build-scripts/build/otdt_prerequisites.sh
+++ releng/build-scripts/build/otdt_prerequisites.sh
@@ -52,14 +52,14 @@ fi
 
 # VERSIONS:
 # Eclipse SDK build identifier (used for substitution in otdt.map.in etc.):
-SDK_QUALIFIER=I20150429-1330
+SDK_QUALIFIER=I20150429-2000
 
 # Architecture (as used by OSGi):
 ARCH=`arch`
 
 # used only locally (components of the ECLIPSE_SDK_TGZ path):
-EVERSION=I20150429-1330
-DROP=${BASEDIR}/drops4/I20150429-1330
+EVERSION=I20150429-2000
+DROP=${BASEDIR}/drops4/I20150429-2000
 
 # EXPORT: archive file of the base eclipse SDK build:
 ECLIPSE_SDK_TGZ=${DROP}/eclipse-SDK-${EVERSION}-linux-gtk-${ARCH}.tar.gz
