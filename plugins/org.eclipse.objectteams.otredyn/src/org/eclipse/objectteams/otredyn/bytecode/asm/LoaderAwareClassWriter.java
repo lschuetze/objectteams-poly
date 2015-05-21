1baf601640a466ef3dc78a2117922359e60bda61
Stephan Herrmann <stephan.herrmann@berlin.de>
2015-05-21 23:30:06 +0200

    Bug 467913 - [otdre] "Instruction type does not match stack map" on woven TypeHierarchy

--- plugins/org.eclipse.objectteams.otredyn/src/org/eclipse/objectteams/otredyn/bytecode/asm/LoaderAwareClassWriter.java
+++ plugins/org.eclipse.objectteams.otredyn/src/org/eclipse/objectteams/otredyn/bytecode/asm/LoaderAwareClassWriter.java
@@ -16,9 +16,14 @@
 package org.eclipse.objectteams.otredyn.bytecode.asm;
 
 import java.io.InputStream;
+import java.util.HashSet;
+import java.util.Set;
 
+import org.eclipse.objectteams.otredyn.bytecode.asm.ASMByteCodeAnalyzer.ClassInformation;
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.ClassWriter;
+
+import static org.eclipse.objectteams.otredyn.transformer.names.ClassNames.OBJECT_SLASH;
 
 /**
  * Variant of its superclass which strictly avoids the use of Class.forName(),
@@ -28,40 +33,83 @@ public class LoaderAwareClassWriter extends ClassWriter {
 
 	// Only use as a resource loader!
 	private ClassLoader loader;
+	private ASMByteCodeAnalyzer analyzer; // hopefully caching loaded classes per class-being-written is sufficient for performance
 
 	public LoaderAwareClassWriter(ClassReader reader, int computeFrames, ClassLoader loader) {
 		super(reader, computeFrames);
 		this.loader = loader;
+		this.analyzer = new ASMByteCodeAnalyzer(false);
 	}
 	
 	@Override
 	protected String getCommonSuperClass(String type1, String type2) {
+		// simple cases first:
+		if (type1.equals(type2))
+			return type1;
+		if (type1.equals(OBJECT_SLASH) || type2.equals(OBJECT_SLASH))
+			return OBJECT_SLASH;
+		
+		// need to load class bytes:
+		InputStream s1;
+		InputStream s2;
         try {
-    		InputStream s1 = this.loader.getResourceAsStream(type1+".class");
-    		InputStream s2 = this.loader.getResourceAsStream(type2+".class");
+    		s1 = this.loader.getResourceAsStream(type1+".class");
+    		s2 = this.loader.getResourceAsStream(type2+".class");
         } catch (Exception e) {
             throw new RuntimeException(e.toString());
         }
-        // FIXME: analyse super types from the above streams!
-        // consider using (a copy of?) org.eclipse.objectteams.internal.osgi.weaving.ASMByteCodeAnalyzer
-        return type2;
-//        if (c.isAssignableFrom(d)) {
-//        	System.err.println("answer "+type1);
-//            return type1;
-//        }
-//        if (d.isAssignableFrom(c)) {
-//        	System.err.println("answer "+type2);
-//            return type2;
-//        }
-//        System.err.println("not assignable");
-//        if (c.isInterface() || d.isInterface()) {
-//            return "java/lang/Object";
-//        } else {
-//            do {
-//                c = c.getSuperclass();
-//            } while (!c.isAssignableFrom(d));
-//            return c.getName().replace('.', '/');
-//        }
+        ClassInformation ci1 = this.analyzer.getClassInformation(s1, type1);
+        ClassInformation ci2 = this.analyzer.getClassInformation(s2, type2);
+        if (ci1 == null || ci2 == null)
+        	return OBJECT_SLASH;
+
+        // do a breadth-first search: each iteration adds just one more level of super types:
+        Set<String> allTypes1 = new HashSet<String>();
+        Set<String> allTypes2 = new HashSet<String>();
+        allTypes1.add(type1);
+        allTypes2.add(type2);
+        Set<String> newTypes1 = getDirectSupers(ci1);
+        Set<String> newTypes2 = getDirectSupers(ci2);
+        while (true) {
+        	if (newTypes1.isEmpty() && newTypes2.isEmpty())
+        		return OBJECT_SLASH;
+        	for (String newType1 : newTypes1)
+        		if (allTypes2.contains(newType1))
+        			return newType1;
+        	for (String newType2 : newTypes2)
+        		if (allTypes1.contains(newType2))
+        			return newType2;
+        	allTypes1.addAll(newTypes1);
+        	allTypes2.addAll(newTypes2);
+        	newTypes1 = getDirectSupers(newTypes1);
+        	newTypes2 = getDirectSupers(newTypes2);
+        }
+	}
+
+	private Set<String> getDirectSupers(Set<String> types) {
+		Set<String> result = new HashSet<String>();
+		for (String type : types) {
+			InputStream s;
+			try {
+				s = this.loader.getResourceAsStream(type+".class");
+	        } catch (Exception e) {
+	            throw new RuntimeException(e.toString());
+			}
+			ClassInformation ci = this.analyzer.getClassInformation(s, type);
+			if (ci != null)
+				result.addAll(getDirectSupers(ci));
+		}
+		return result;
+	}
+
+	private Set<String> getDirectSupers(ClassInformation ci) {
+		Set<String> result = new HashSet<String>();
+		String superClass = ci.getSuperClassName();
+		if (superClass != null && !superClass.equals(OBJECT_SLASH)) // avoid prematurely answering j.l.Object
+			result.add(superClass);
+		for (String ifc : ci.getSuperInterfaceNames())
+			result.add(ifc);
+		return result;
 	}
 
 }
