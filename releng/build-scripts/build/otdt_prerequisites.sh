92e58756360578759c68a78260fe47fa9aac3038
Stephan Herrmann <stephan.herrmann@berlin.de>
2015-07-19 18:27:13 +0200

    Build against I20150714-0800 & 1st versions bump up

--- releng/build-scripts/build/otdt_prerequisites.sh
+++ releng/build-scripts/build/otdt_prerequisites.sh
@@ -52,14 +52,14 @@ fi
 
 # VERSIONS:
 # Eclipse SDK build identifier (used for substitution in otdt.map.in etc.):
-SDK_QUALIFIER=I20150603-2000
+SDK_QUALIFIER=I20150714-0800
 
 # Architecture (as used by OSGi):
 ARCH=`arch`
 
 # used only locally (components of the ECLIPSE_SDK_TGZ path):
-EVERSION=4.5RC4
-DROP=${BASEDIR}/drops4/S-4.5RC4-201506032000
+EVERSION=I20150714-0800
+DROP=${BASEDIR}/drops4/I20150714-0800
 
 # EXPORT: archive file of the base eclipse SDK build:
 ECLIPSE_SDK_TGZ=${DROP}/eclipse-SDK-${EVERSION}-linux-gtk-${ARCH}.tar.gz
@@ -68,5 +68,5 @@ ECLIPSE_SDK_TGZ=${DROP}/eclipse-SDK-${EVERSION}-linux-gtk-${ARCH}.tar.gz
 ECLIPSE_TESTLIB_ZIP=${DROP}/eclipse-test-framework-${EVERSION}.zip
 
 # EXPORT: where to find previously published plugins&features:
-PUBLISHED_UPDATES=${HOME}/downloads/objectteams/updates/ot2.3
+PUBLISHED_UPDATES=${HOME}/downloads/objectteams/updates/ot2.4
 
