8033b7b0591e4ea642a9a1cad1ffdf3a3590aa77
Stephan Herrmann <stephan.herrmann@berlin.de>
2016-03-19 16:23:38 +0100

    Releng: build against 4.6M6

--- releng/build-scripts/build/otdt_prerequisites.sh
+++ releng/build-scripts/build/otdt_prerequisites.sh
@@ -52,14 +52,14 @@ fi
 
 # VERSIONS:
 # Eclipse SDK build identifier (used for substitution in otdt.map.in etc.):
-SDK_QUALIFIER=I20160128-2000
+SDK_QUALIFIER=I20160317-0200
 
 # Architecture (as used by OSGi):
 ARCH=`arch`
 
 # used only locally (components of the ECLIPSE_SDK_TGZ path):
-EVERSION=4.6M5
-DROP=${BASEDIR}/drops4/S-4.6M5-201601282000/
+EVERSION=4.6M6
+DROP=${BASEDIR}/drops4/S-4.6M6-201603170200/
 
 # EXPORT: archive file of the base eclipse SDK build:
 ECLIPSE_SDK_TGZ=${DROP}/eclipse-SDK-${EVERSION}-linux-gtk-${ARCH}.tar.gz
