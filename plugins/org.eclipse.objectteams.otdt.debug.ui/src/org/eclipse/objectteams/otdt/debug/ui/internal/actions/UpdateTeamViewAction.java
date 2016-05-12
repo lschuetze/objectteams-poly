49479139e7570d9fd242c1d74e9882d79e8d42ca
Stephan Herrmann <stephan.herrmann@berlin.de>
2016-05-12 23:27:09 +0200

    Bug 487382: [debug] CCE from Team Monitor

--- plugins/org.eclipse.objectteams.otdt.debug.ui/src/org/eclipse/objectteams/otdt/debug/ui/internal/actions/UpdateTeamViewAction.java
+++ plugins/org.eclipse.objectteams.otdt.debug.ui/src/org/eclipse/objectteams/otdt/debug/ui/internal/actions/UpdateTeamViewAction.java
@@ -1,16 +1,15 @@
 /**********************************************************************
  * This file is part of "Object Teams Development Tooling"-Software
  * 
- * Copyright 2004, 2006 Fraunhofer Gesellschaft, Munich, Germany,
+ * Copyright 2004, 2016 Fraunhofer Gesellschaft, Munich, Germany,
  * for its Fraunhofer Institute and Computer Architecture and Software
  * Technology (FIRST), Berlin, Germany and Technical University Berlin,
- * Germany.
+ * Germany, and others.
  * 
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * $Id: UpdateTeamViewAction.java 23432 2010-02-03 23:13:42Z stephan $
  * 
  * Please visit http://www.objectteams.org for updates and contact.
  * 
@@ -21,7 +20,6 @@
 package org.eclipse.objectteams.otdt.debug.ui.internal.actions;
 
 import org.eclipse.jface.action.Action;
-import org.eclipse.objectteams.otdt.debug.OTDebugElementsContainer;
 import org.eclipse.objectteams.otdt.debug.ui.OTDebugImages;
 import org.eclipse.objectteams.otdt.debug.ui.views.TeamView;
 
@@ -42,7 +40,7 @@ public class UpdateTeamViewAction extends Action
 	public void run()
 	{
 		_teamView.setUpdatePermanently(isChecked());
-		OTDebugElementsContainer container = (OTDebugElementsContainer)_teamView.getViewer().getInput();
+		Object container = _teamView.getViewer().getInput();
 		if(isChecked())
 		{
 			_teamView.getViewer().setInput(container);
