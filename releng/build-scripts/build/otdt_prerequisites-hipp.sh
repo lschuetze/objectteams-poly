c35c481cfc10d93db9b0c6861de5efd9618a7ce2
Stephan Herrmann <stephan.herrmann@berlin.de>
2016-12-08 19:53:42 +0100

    Releng: build against I20161208-0830 for Oxygen M4

--- releng/build-scripts/build/otdt_prerequisites-hipp.sh
+++ releng/build-scripts/build/otdt_prerequisites-hipp.sh
@@ -52,14 +52,14 @@ NICE="10"
 
 # VERSIONS:
 # Eclipse SDK build identifier (used for substitution in otdt.map.in etc.):
-SDK_QUALIFIER=${SDK_QUALIFIER:="I20161202-2000"}
+SDK_QUALIFIER=${SDK_QUALIFIER:="I20161208-0830"}
 
 # Architecture (as used by OSGi):
 ARCH=`arch`
 
 # used only locally (components of the ECLIPSE_SDK_TGZ path):
-EVERSION=${EVERSION:="I20161202-2000"}
-DROP=${DROPS4}/${DROP:="I20161202-2000"}
+EVERSION=${EVERSION:="I20161208-0830"}
+DROP=${DROPS4}/${DROP:="I20161208-0830"}
 
 # EXPORT: archive file of the base eclipse SDK build:
 ECLIPSE_SDK_TGZ=${DROP}/eclipse-SDK-${EVERSION}-linux-gtk-${ARCH}.tar.gz
