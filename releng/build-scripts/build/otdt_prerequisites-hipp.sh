35b43e6f689d2c74a3f2d0f6d99454eddb8ff1ba
Stephan Herrmann <stephan.herrmann@berlin.de>
2017-01-02 22:17:17 +0100

    Releng: build against I20170101-2000

--- releng/build-scripts/build/otdt_prerequisites-hipp.sh
+++ releng/build-scripts/build/otdt_prerequisites-hipp.sh
@@ -52,14 +52,14 @@ NICE="10"
 
 # VERSIONS:
 # Eclipse SDK build identifier (used for substitution in otdt.map.in etc.):
-SDK_QUALIFIER=${SDK_QUALIFIER:="I20161208-0830"}
+SDK_QUALIFIER=${SDK_QUALIFIER:="I20170101-2000"}
 
 # Architecture (as used by OSGi):
 ARCH=`arch`
 
 # used only locally (components of the ECLIPSE_SDK_TGZ path):
-EVERSION=${EVERSION:="I20161208-0830"}
-DROP=${DROPS4}/${DROP:="I20161208-0830"}
+EVERSION=${EVERSION:="I20170101-2000"}
+DROP=${DROPS4}/${DROP:="I20170101-2000"}
 
 # EXPORT: archive file of the base eclipse SDK build:
 ECLIPSE_SDK_TGZ=${DROP}/eclipse-SDK-${EVERSION}-linux-gtk-${ARCH}.tar.gz
