f06e713708d4ef94b31bed7371e789fb9e91bf52
Stephan Herrmann <stephan.herrmann@berlin.de>
2017-02-02 23:46:16 +0100

    Releng: the M5 I-build has been renamed

--- releng/build-scripts/build/otdt_prerequisites-hipp.sh
+++ releng/build-scripts/build/otdt_prerequisites-hipp.sh
@@ -58,8 +58,8 @@ SDK_QUALIFIER=${SDK_QUALIFIER:="I20170126-1030"}
 ARCH=`arch`
 
 # used only locally (components of the ECLIPSE_SDK_TGZ path):
-EVERSION=${EVERSION:="I20170126-1030"}
-DROP=${DROPS4}/${DROP:="I20170126-1030"}
+EVERSION=${EVERSION:="4.7M5"}
+DROP=${DROPS4}/${DROP:="S-4.7M5-201701261030"}
 
 # EXPORT: archive file of the base eclipse SDK build:
 ECLIPSE_SDK_TGZ=${DROP}/eclipse-SDK-${EVERSION}-linux-gtk-${ARCH}.tar.gz
