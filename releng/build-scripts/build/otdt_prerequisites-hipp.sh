b94648528b25ab8d6d850fb8cdc79df9999a64c3
Stephan Herrmann <stephan.herrmann@berlin.de>
2017-06-06 16:20:00 +0200

    Releng: final steps to build Oxygen RC3 - copyright in messages.properties and about.ini - orbit release version - feature version otequinox.otre - build against S-4.7RC3-201705312000/ - ot update site is: updates/ot2.6 - open otequinox.branding for Bug 513121

--- releng/build-scripts/build/otdt_prerequisites-hipp.sh
+++ releng/build-scripts/build/otdt_prerequisites-hipp.sh
@@ -52,14 +52,14 @@ NICE="10"
 
 # VERSIONS:
 # Eclipse SDK build identifier (used for substitution in otdt.map.in etc.):
-SDK_QUALIFIER=${SDK_QUALIFIER:="I20170510-2000"}
+SDK_QUALIFIER=${SDK_QUALIFIER:="I20170531-2000"}
 
 # Architecture (as used by OSGi):
 ARCH=`arch`
 
 # used only locally (components of the ECLIPSE_SDK_TGZ path):
-EVERSION=${EVERSION:="I20170510-2000"}
-DROP=${DROPS4}/${DROP:="I20170510-2000"}
+EVERSION=${EVERSION:="S-4.7RC3"}
+DROP=${DROPS4}/${DROP:="S-4.7RC3-201705312000/"}
 
 # EXPORT: archive file of the base eclipse SDK build:
 ECLIPSE_SDK_TGZ=${DROP}/eclipse-SDK-${EVERSION}-linux-gtk-${ARCH}.tar.gz
