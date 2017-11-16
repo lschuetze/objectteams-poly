20bfcd97e4ce8f93e9268c8a2f24e107167f6ee9
Stephan Herrmann <stephan.herrmann@berlin.de>
2017-11-16 18:32:22 +0100

    Bug 510072: Scripts & tool for publishing to Maven Central should migrate to releng git - update for Oxygen.1a

--- releng/EnrichPoms/properties.sh
+++ releng/EnrichPoms/properties.sh
@@ -12,12 +12,12 @@
 # ECLIPSE:
 APP_NAME_P2DIRECTOR=org.eclipse.equinox.p2.director
 DROPS4=/home/data/httpd/download.eclipse.org/eclipse/downloads/drops4
-SDK_BUILD_DIR=R-4.7-201706120950
-SDK_VERSION=4.7
+SDK_BUILD_DIR=R-4.7.1a-201710090410
+SDK_VERSION=4.7.1a
 FILE_ECLIPSE=${DROPS4}/${SDK_BUILD_DIR}/eclipse-SDK-${SDK_VERSION}-linux-gtk-x86_64.tar.gz
 
 # JDT / ECJ:
-ECJ_VERSION=3.13.0
+ECJ_VERSION=3.13.50
 
 # AGGREGATOR:
 IU_AGG_PRODUCT=org.eclipse.cbi.p2repo.cli.product
