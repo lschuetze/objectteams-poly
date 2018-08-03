221ad3a8bccbce1355ca1bb9d9b74d902f95a873
Stephan Herrmann <stephan.herrmann@berlin.de>
2018-08-03 23:27:55 +0200

    Build against S-4.9M2-201808012000

--- releng/build-scripts/build/otdt_prerequisites-hipp.sh
+++ releng/build-scripts/build/otdt_prerequisites-hipp.sh
@@ -53,14 +53,14 @@ NICE="10"
 
 # VERSIONS:
 # Eclipse SDK build identifier (used for substitution in otdt.map.in etc.):
-SDK_QUALIFIER=${SDK_QUALIFIER:="I20180725-2000"}
+SDK_QUALIFIER=${SDK_QUALIFIER:="I20180801-2000"}
 
 # Architecture (as used by OSGi):
 ARCH=`arch`
 
 # used only locally (components of the ECLIPSE_SDK_TGZ path):
-EVERSION=${EVERSION:="I20180725-2000"}
-DROP=${DROPS4}/${DROP:="I20180725-2000"}
+EVERSION=${EVERSION:="I20180801-2000"}
+DROP=${DROPS4}/${DROP:="S-4.9M2-201808012000"}
 
 # EXPORT: archive file of the base eclipse SDK build:
 ECLIPSE_SDK_TGZ=${DROP}/eclipse-SDK-${EVERSION}-linux-gtk-${ARCH}.tar.gz
