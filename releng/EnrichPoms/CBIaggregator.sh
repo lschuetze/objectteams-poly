0def6d578a8c3b4f296f2960b17e6f480c3366c4
Stephan Herrmann <stephan.herrmann@berlin.de>
2018-10-27 23:01:53 +0200

    Bug 510072: Scripts & tool for publishing to Maven Central should migrate to releng git
    - include building of EnrichPoms.jar into the main script.
      - fix location of sourceBundles.txt
    
    Change-Id: I3ffb261303bf6ecc7ffaa61f8057e4d3d46df86e

--- releng/EnrichPoms/CBIaggregator.sh
+++ releng/EnrichPoms/CBIaggregator.sh
@@ -365,7 +365,7 @@ function buildSourceJar() {
 while read line
 do
     buildSourceJar $line 
-done < ${WORKSPACE}/sourceBundles.txt
+done < ${WORKSPACE}/work/sourceBundles.txt
 
 # copy ecj-src from build output (NB: we are mapping from SDK version (4.x) back to ECJ version (3.x)):
 /bin/cp ${DROPS4}/${SDK_BUILD_DIR}/ecjsrc-${SDK_VERSION}.jar org/eclipse/jdt/ecj/${ECJ_VERSION}/ecj-${ECJ_VERSION}-sources.jar
