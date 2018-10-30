e2be43953bc2de1c94661fffef56df58576b4cbe
Stephan Herrmann <stephan.herrmann@berlin.de>
2018-10-30 21:23:17 +0100

    Revert tweaks from bug 540495
    Change-Id: I02208ebb4a62e51fd56a4eccfdaa8be6c1416c42

--- releng/EnrichPoms/publishJDT.sh
+++ releng/EnrichPoms/publishJDT.sh
@@ -68,8 +68,7 @@ function same_as_baseline() {
 	return 1
 }
 
-#for pomFile in org/eclipse/jdt/*/*/*.pom
-for pomFile in org/eclipse/jdt/ecj/*/*.pom
+for pomFile in org/eclipse/jdt/*/*/*.pom
 do
   if same_as_baseline $pomFile
   then
