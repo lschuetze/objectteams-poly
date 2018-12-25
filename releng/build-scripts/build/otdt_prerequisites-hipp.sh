c97a0c4575fa495005f268229ee6d883494ef3eb
Stephan Herrmann <stephan.herrmann@berlin.de>
2018-12-25 19:23:32 +0100

    Releng: Stabilize after 2.7.2: - freeze some versions - build against 4.10 R

--- releng/build-scripts/build/otdt_prerequisites-hipp.sh
+++ releng/build-scripts/build/otdt_prerequisites-hipp.sh
@@ -59,8 +59,8 @@ SDK_QUALIFIER=${SDK_QUALIFIER:="I20181206-0815"}
 ARCH=`arch`
 
 # used only locally (components of the ECLIPSE_SDK_TGZ path):
-EVERSION=${EVERSION:="I20181206-0815"}
-DROP=${DROPS4}/${DROP:="I20181206-0815"}
+EVERSION=${EVERSION:="4.10"}
+DROP=${DROPS4}/${DROP:="R-4.10-201812060815"}
 
 # EXPORT: archive file of the base eclipse SDK build:
 ECLIPSE_SDK_TGZ=${DROP}/eclipse-SDK-${EVERSION}-linux-gtk-${ARCH}.tar.gz
