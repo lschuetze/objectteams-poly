338aa2d9e1d4553cf9f7eb7aec050faf300776c8
Stephan Herrmann <stephan.herrmann@berlin.de>
2019-03-21 15:18:09 +0100

    regenerate parser after J12 merge

--- org.eclipse.jdt.core/compiler/org/eclipse/jdt/internal/compiler/parser/ParserBasicInformation.java
+++ org.eclipse.jdt.core/compiler/org/eclipse/jdt/internal/compiler/parser/ParserBasicInformation.java
@@ -18,22 +18,22 @@ package org.eclipse.jdt.internal.compiler.parser;
 public interface ParserBasicInformation {
     public final static int
 
-      ERROR_SYMBOL      = 146,
+      ERROR_SYMBOL      = 147,
       MAX_NAME_LENGTH   = 41,
-      NUM_STATES        = 1424,
+      NUM_STATES        = 1438,
 
-      NT_OFFSET         = 146,
-      SCOPE_UBOUND      = 379,
-      SCOPE_SIZE        = 380,
-      LA_STATE_OFFSET   = 19607,
+      NT_OFFSET         = 147,
+      SCOPE_UBOUND      = 386,
+      SCOPE_SIZE        = 387,
+      LA_STATE_OFFSET   = 19669,
       MAX_LA            = 1,
-      NUM_RULES         = 1033,
-      NUM_TERMINALS     = 146,
-      NUM_NON_TERMINALS = 466,
-      NUM_SYMBOLS       = 612,
-      START_STATE       = 1277,
-      EOFT_SYMBOL       = 64,
-      EOLT_SYMBOL       = 64,
-      ACCEPT_ACTION     = 19606,
-      ERROR_ACTION      = 19607;
+      NUM_RULES         = 1049,
+      NUM_TERMINALS     = 147,
+      NUM_NON_TERMINALS = 475,
+      NUM_SYMBOLS       = 622,
+      START_STATE       = 1305,
+      EOFT_SYMBOL       = 66,
+      EOLT_SYMBOL       = 66,
+      ACCEPT_ACTION     = 19668,
+      ERROR_ACTION      = 19669;
 }
