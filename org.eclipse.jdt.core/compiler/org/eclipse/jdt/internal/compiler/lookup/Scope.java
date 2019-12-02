607e1636c4d5182b1d0e79dfab0e7484bf7903ac
Stephan Herrmann <stephan.herrmann@berlin.de>
2019-12-02 22:31:36 +0100

    Update jdt.core to S4_14_0_RC1

--- org.eclipse.jdt.core/compiler/org/eclipse/jdt/internal/compiler/lookup/Scope.java
+++ org.eclipse.jdt.core/compiler/org/eclipse/jdt/internal/compiler/lookup/Scope.java
@@ -2748,6 +2748,10 @@ public abstract class Scope {
 				MethodScope methodScope = methodScope();
 				if (!methodScope.isInsideInitializer()){
 					// check method modifiers to see if deprecated
+					ReferenceContext ref = methodScope.referenceContext();
+					if (ref instanceof ModuleDeclaration) {
+						return ((ModuleDeclaration)ref).modifiers;
+					}
 					MethodBinding context = ((AbstractMethodDeclaration)methodScope.referenceContext).binding;
 					if (context != null)
 						return context.modifiers;
