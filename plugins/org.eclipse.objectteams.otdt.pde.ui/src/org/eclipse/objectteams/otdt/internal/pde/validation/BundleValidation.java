a1053cbb3dbed94e0e3185f1ffa4640d5464ddc8
Stephan Herrmann <stephan.herrmann@berlin.de>
2020-07-16 13:10:17 +0200

    Releng: all test versions are take from finalPluginsVersions.properties
    + try to do without manually maintained version mapping
      - obsolete since commit 32ed6afa05944f9d753b1aefa94a594dda963124 ?
    
    + polish of previous commit

--- plugins/org.eclipse.objectteams.otdt.pde.ui/src/org/eclipse/objectteams/otdt/internal/pde/validation/BundleValidation.java
+++ plugins/org.eclipse.objectteams.otdt.pde.ui/src/org/eclipse/objectteams/otdt/internal/pde/validation/BundleValidation.java
@@ -617,7 +617,7 @@ public team class BundleValidation
 		String packageName;
 		String export; // extended version with ot-aspect-host attribute
 		public ExportAspectPackageResolution(IMarker marker) {
-			super(marker, AbstractPDEMarkerResolution.CREATE_TYPE); //$NON-NLS-1$
+			super(marker, AbstractPDEMarkerResolution.CREATE_TYPE);
 			this.packageName = marker.getAttribute("package", null); //$NON-NLS-1$
 			this.export = marker.getAttribute("packages", null); //$NON-NLS-1$
 		}
