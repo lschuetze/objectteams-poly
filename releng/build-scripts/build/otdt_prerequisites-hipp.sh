9e853854a6b190fbbe3eda216153a0c2a852e32f
Stephan Herrmann <stephan.herrmann@berlin.de>
2020-11-28 21:16:42 +0100

    Build against S-4.18RC1-202011251800

--- releng/build-scripts/build/otdt_prerequisites-hipp.sh
+++ releng/build-scripts/build/otdt_prerequisites-hipp.sh
@@ -58,11 +58,11 @@ ARCH=`arch`
 
 # VERSIONS:
 # Eclipse SDK build identifier (used for substitution in otdt.map.in etc.):
-SDK_QUALIFIER=${SDK_QUALIFIER:="I20201117-0600"}
+SDK_QUALIFIER=${SDK_QUALIFIER:="I20201125-1800"}
 
 # used only locally (components of the ECLIPSE_SDK_TGZ path):
-EVERSION=${EVERSION:="I20201117-0600"}
-DROP=${DROPS4}/${DROP:="I20201117-0600"}
+EVERSION=${EVERSION:="4.18RC1"}
+DROP=${DROPS4}/${DROP:="S-4.18RC1-202011251800"}
 
 # EXPORT: archive file of the base eclipse SDK build:
 ECLIPSE_SDK_TGZ=${DROP}/eclipse-SDK-${EVERSION}-linux-gtk-${ARCH}.tar.gz
