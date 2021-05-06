3a584613976e62aba20e096e0395cc807c25807a
Stephan Herrmann <stephan.herrmann@berlin.de>
2021-05-06 21:02:02 +0200

    Releng: build against I20210503-1800

--- releng/build-scripts/build/otdt_prerequisites-hipp.sh
+++ releng/build-scripts/build/otdt_prerequisites-hipp.sh
@@ -58,11 +58,11 @@ ARCH=`arch`
 
 # VERSIONS:
 # Eclipse SDK build identifier (used for substitution in otdt.map.in etc.):
-SDK_QUALIFIER=${SDK_QUALIFIER:="I20210320-0650"}
+SDK_QUALIFIER=${SDK_QUALIFIER:="I20210503-1800"}
 
 # used only locally (components of the ECLIPSE_SDK_TGZ path):
-EVERSION=${EVERSION:="I20210320-0650"}
-DROP=${DROPS4}/${DROP:="I20210320-0650"}
+EVERSION=${EVERSION:="I20210503-1800"}
+DROP=${DROPS4}/${DROP:="I20210503-1800"}
 
 # EXPORT: archive file of the base eclipse SDK build:
 ECLIPSE_SDK_TGZ=${DROP}/eclipse-SDK-${EVERSION}-linux-gtk-${ARCH}.tar.gz
