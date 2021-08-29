eff9e5423288cb26f0f3b600830d9b870ce2f6af
Stephan Herrmann <stephan.herrmann@berlin.de>
2021-08-29 12:19:44 +0200

    Releng: build against S-4.21RC1-202108251800

--- releng/build-scripts/build/otdt_prerequisites-hipp.sh
+++ releng/build-scripts/build/otdt_prerequisites-hipp.sh
@@ -58,11 +58,11 @@ ARCH=`arch`
 
 # VERSIONS:
 # Eclipse SDK build identifier (used for substitution in otdt.map.in etc.):
-SDK_QUALIFIER=${SDK_QUALIFIER:="I20210818-1800"}
+SDK_QUALIFIER=${SDK_QUALIFIER:="I20210825-1800"}
 
 # used only locally (components of the ECLIPSE_SDK_TGZ path):
-EVERSION=${EVERSION:="I20210818-1800"}
-DROP=${DROPS4}/${DROP:="I20210818-1800"}
+EVERSION=${EVERSION:="4.21RC1"}
+DROP=${DROPS4}/${DROP:="S-4.21RC1-202108251800"}
 
 # EXPORT: archive file of the base eclipse SDK build:
 ECLIPSE_SDK_TGZ=${DROP}/eclipse-SDK-${EVERSION}-linux-gtk-${ARCH}.tar.gz
